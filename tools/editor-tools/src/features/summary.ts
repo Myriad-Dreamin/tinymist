27ab53adaa3a6beb81da834ce126abddbfef28c9
Myriad-Dreamin <camiyoru@gmail.com>
2024-04-05 11:15:21 +0800

    dev: humanize font variant to show

--- tools/editor-tools/src/features/summary.ts
+++ tools/editor-tools/src/features/summary.ts
@@ -68,6 +68,23 @@ export const Summary = () => {
       font.usesScale,
       " use(s).",
       br(),
+      code("Variant"),
+      ": ",
+      code(
+        font.style === "normal" || !font.style
+          ? ""
+          : `${humanStyle(font.style)}, `,
+        span(
+          { title: `Weight ${font.weight || 400}` },
+          `${humanWeight(font.weight)} Weight`
+        ),
+        ", ",
+        span(
+          { title: `Stretch ${(font.stretch || 1000) / 10}%` },
+          `${humanStretch(font.stretch)} Stretch`
+        )
+      ),
+      br(),
       code("PostScriptName"),
       ": ",
       code(font.postscriptName),
@@ -276,6 +293,9 @@ interface AnnotatedContent {
 
 interface FontInfo {
   name: string;
+  style?: string;
+  weight?: number;
+  stretch?: number;
   postscriptName: string;
   family?: string;
   fullName?: string;
@@ -353,4 +373,112 @@ function humanSize(size: number) {
     unit++;
   }
   return `${size.toFixed(2)} ${units[unit]}`;
+}
+
+function almost(value: number, target: number, threshold = 0.01) {
+  return Math.abs(value - target) < threshold;
+}
+
+function humanStyle(style?: string) {
+  if (!style) {
+    return "Regular";
+  }
+
+  if (style === "italic") {
+    return "Italic";
+  }
+
+  if (style === "oblique") {
+    return "Oblique";
+  }
+
+  return `Style ${style}`;
+}
+
+function humanWeight(weight?: number) {
+  if (!weight) {
+    return "Regular";
+  }
+
+  if (almost(weight, 100)) {
+    return "Thin";
+  }
+
+  if (almost(weight, 200)) {
+    return "Extra Light";
+  }
+
+  if (almost(weight, 300)) {
+    return "Light";
+  }
+
+  if (almost(weight, 400)) {
+    return "Regular";
+  }
+
+  if (almost(weight, 500)) {
+    return "Medium";
+  }
+
+  if (almost(weight, 600)) {
+    return "Semibold";
+  }
+
+  if (almost(weight, 700)) {
+    return "Bold";
+  }
+
+  if (almost(weight, 800)) {
+    return "Extra Bold";
+  }
+
+  if (almost(weight, 900)) {
+    return "Black";
+  }
+
+  return `Weight ${weight}`;
+}
+
+function humanStretch(stretch?: number) {
+  if (!stretch) {
+    return "Normal";
+  }
+
+  if (almost(stretch, 500)) {
+    return "Ultra-condensed";
+  }
+
+  if (almost(stretch, 625)) {
+    return "Extra-condensed";
+  }
+
+  if (almost(stretch, 750)) {
+    return "Condensed";
+  }
+
+  if (almost(stretch, 875)) {
+    return "Semi-condensed";
+  }
+
+  if (almost(stretch, 1000)) {
+    return "Normal";
+  }
+
+  if (almost(stretch, 1125)) {
+    return "Semi-expanded";
+  }
+
+  if (almost(stretch, 1250)) {
+    return "Expanded";
+  }
+
+  if (almost(stretch, 1500)) {
+    return "Extra-expanded";
+  }
+
+  if (almost(stretch, 2000)) {
+    return "Ultra-expanded";
+  }
+
+  return `${stretch}`;
 }
