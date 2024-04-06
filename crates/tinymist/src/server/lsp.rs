f60d78a3ca6e68e24c75d30a5a3ab259cf213a9c
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2024-04-06 09:24:42 +0800

    dev: clear analysis cache (#165)
    * dev: clear analysis cache
    
    * dev: remove an extra clear

--- crates/tinymist/src/server/lsp.rs
+++ crates/tinymist/src/server/lsp.rs
@@ -678,6 +678,12 @@ impl TypstLanguageServer {
     /// Errors if the cache could not be cleared.
     pub fn clear_cache(&self, _arguments: Vec<JsonValue>) -> LspResult<JsonValue> {
         comemo::evict(0);
+        for v in Some(self.primary())
+            .into_iter()
+            .chain(self.dedicates.iter().map(|v| v.compiler()))
+        {
+            v.clear_cache();
+        }
         Ok(JsonValue::Null)
     }
 
