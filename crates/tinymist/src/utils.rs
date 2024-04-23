5379b05abdcdedaf7747fdabbca01045516e5c47
QuarticCat <QuarticCat@pm.me>
2024-04-23 11:14:06 +0800

    feat: use rayon::scope for threaded_receive

--- crates/tinymist/src/utils.rs
+++ crates/tinymist/src/utils.rs
@@ -1,5 +1,3 @@
-use std::thread;
-
 use tokio::sync::oneshot;
 use typst_ts_core::error::prelude::*;
 use typst_ts_core::Error;
@@ -8,14 +6,16 @@ pub fn threaded_receive<T: Send>(f: oneshot::Receiver<T>) -> Result<T, Error> {
     // get current async handle
     if let Ok(e) = tokio::runtime::Handle::try_current() {
         // todo: remove blocking
-        return thread::scope(|s| {
-            s.spawn(move || {
-                e.block_on(f)
-                    .map_err(map_string_err("failed to sync_render"))
+        let mut ret = None;
+        rayon::scope(|s| {
+            s.spawn(|_| {
+                ret = Some(
+                    e.block_on(f)
+                        .map_err(map_string_err("failed to sync_render")),
+                )
             })
-            .join()
-            .map_err(|_| error_once!("failed to join"))?
         });
+        return ret.unwrap();
     }
 
     f.blocking_recv()
