9648d622de7ebed43db0c2c814a4d4da1646a4c0
Myriad-Dreamin <camiyoru@gmail.com>
2024-04-27 09:18:49 +0800

    dev: improve switch

--- crates/tinymist-query/src/code_context.rs
+++ crates/tinymist-query/src/code_context.rs
@@ -73,140 +73,37 @@ impl SyntaxRequest for InteractCodeContextRequest {
                     let mut leaf = leaf.as_ref();
                     let mode = loop {
                         log::info!("leaf for context: {:?}", leaf);
+                        use SyntaxKind::*;
                         if let Some(t) = leaf {
                             match t.kind() {
-                                SyntaxKind::LineComment | SyntaxKind::BlockComment => {
-                                    break InterpretMode::Comment
-                                }
-                                SyntaxKind::Raw => break InterpretMode::Raw,
-                                SyntaxKind::Str => break InterpretMode::String,
-                                SyntaxKind::Markup => break InterpretMode::Markup,
-                                SyntaxKind::FuncCall
-                                | SyntaxKind::Unary
-                                | SyntaxKind::Binary
-                                | SyntaxKind::Parenthesized
-                                | SyntaxKind::Dict
-                                | SyntaxKind::Array
-                                | SyntaxKind::Code => break InterpretMode::Code,
-                                SyntaxKind::Equation | SyntaxKind::Math => {
-                                    break InterpretMode::Math
-                                }
-                                SyntaxKind::Text => break InterpretMode::Markup,
-                                SyntaxKind::Space => {}
-                                SyntaxKind::Linebreak => {}
-                                SyntaxKind::Parbreak => {}
-                                SyntaxKind::Escape => {}
-                                SyntaxKind::Shorthand => {}
-                                SyntaxKind::SmartQuote => {}
-                                SyntaxKind::Strong => break InterpretMode::Markup,
-                                SyntaxKind::Emph => break InterpretMode::Markup,
-                                SyntaxKind::RawLang => {}
-                                SyntaxKind::RawDelim => {}
-                                SyntaxKind::RawTrimmed => {}
-                                SyntaxKind::Link => break InterpretMode::Markup,
-                                SyntaxKind::Label => break InterpretMode::Markup,
-                                SyntaxKind::Ref => break InterpretMode::Markup,
-                                SyntaxKind::RefMarker => break InterpretMode::Markup,
-                                SyntaxKind::Heading => break InterpretMode::Markup,
-                                SyntaxKind::HeadingMarker => break InterpretMode::Markup,
-                                SyntaxKind::ListItem => break InterpretMode::Markup,
-                                SyntaxKind::ListMarker => break InterpretMode::Markup,
-                                SyntaxKind::EnumItem => break InterpretMode::Markup,
-                                SyntaxKind::EnumMarker => break InterpretMode::Markup,
-                                SyntaxKind::TermItem => break InterpretMode::Markup,
-                                SyntaxKind::TermMarker => break InterpretMode::Markup,
-                                SyntaxKind::MathIdent => break InterpretMode::Math,
-                                SyntaxKind::MathAlignPoint => break InterpretMode::Math,
-                                SyntaxKind::MathDelimited => break InterpretMode::Math,
-                                SyntaxKind::MathAttach => break InterpretMode::Math,
-                                SyntaxKind::MathPrimes => break InterpretMode::Math,
-                                SyntaxKind::MathFrac => break InterpretMode::Math,
-                                SyntaxKind::MathRoot => break InterpretMode::Math,
-                                SyntaxKind::Hash => {}
-                                SyntaxKind::LeftBrace => {}
-                                SyntaxKind::RightBrace => {}
-                                SyntaxKind::LeftBracket => {}
-                                SyntaxKind::RightBracket => {}
-                                SyntaxKind::LeftParen => {}
-                                SyntaxKind::RightParen => {}
-                                SyntaxKind::Comma => {}
-                                SyntaxKind::Semicolon => {}
-                                SyntaxKind::Colon => {}
-                                SyntaxKind::Star => {}
-                                SyntaxKind::Underscore => {}
-                                SyntaxKind::Dollar => {}
-                                SyntaxKind::Plus => {}
-                                SyntaxKind::Minus => {}
-                                SyntaxKind::Slash => {}
-                                SyntaxKind::Hat => {}
-                                SyntaxKind::Prime => {}
-                                SyntaxKind::Dot => {}
-                                SyntaxKind::Eq => {}
-                                SyntaxKind::EqEq => {}
-                                SyntaxKind::ExclEq => {}
-                                SyntaxKind::Lt => {}
-                                SyntaxKind::LtEq => {}
-                                SyntaxKind::Gt => {}
-                                SyntaxKind::GtEq => {}
-                                SyntaxKind::PlusEq => {}
-                                SyntaxKind::HyphEq => {}
-                                SyntaxKind::StarEq => {}
-                                SyntaxKind::SlashEq => {}
-                                SyntaxKind::Dots => {}
-                                SyntaxKind::Arrow => {}
-                                SyntaxKind::Root => {}
-                                SyntaxKind::Not => {}
-                                SyntaxKind::And => {}
-                                SyntaxKind::Or => {}
-                                SyntaxKind::None => {}
-                                SyntaxKind::Auto => {}
-                                SyntaxKind::Let => break InterpretMode::Code,
-                                SyntaxKind::Set => break InterpretMode::Code,
-                                SyntaxKind::Show => break InterpretMode::Code,
-                                SyntaxKind::Context => break InterpretMode::Code,
-                                SyntaxKind::If => break InterpretMode::Code,
-                                SyntaxKind::Else => break InterpretMode::Code,
-                                SyntaxKind::For => break InterpretMode::Code,
-                                SyntaxKind::In => break InterpretMode::Code,
-                                SyntaxKind::While => break InterpretMode::Code,
-                                SyntaxKind::Break => break InterpretMode::Code,
-                                SyntaxKind::Continue => break InterpretMode::Code,
-                                SyntaxKind::Return => break InterpretMode::Code,
-                                SyntaxKind::Import => break InterpretMode::Code,
-                                SyntaxKind::Include => break InterpretMode::Code,
-                                SyntaxKind::As => {}
-                                SyntaxKind::Ident => break InterpretMode::Code,
-                                SyntaxKind::Bool => break InterpretMode::Code,
-                                SyntaxKind::Int => break InterpretMode::Code,
-                                SyntaxKind::Float => break InterpretMode::Code,
-                                SyntaxKind::Numeric => break InterpretMode::Code,
-                                SyntaxKind::CodeBlock => break InterpretMode::Code,
-                                SyntaxKind::ContentBlock => break InterpretMode::Markup,
-                                SyntaxKind::Named => {}
-                                SyntaxKind::Keyed => {}
-                                SyntaxKind::FieldAccess => break InterpretMode::Code,
-                                SyntaxKind::Args => break InterpretMode::Code,
-                                SyntaxKind::Spread => break InterpretMode::Code,
-                                SyntaxKind::Closure => break InterpretMode::Code,
-                                SyntaxKind::Params => break InterpretMode::Code,
-                                SyntaxKind::LetBinding => break InterpretMode::Code,
-                                SyntaxKind::SetRule => break InterpretMode::Code,
-                                SyntaxKind::ShowRule => break InterpretMode::Code,
-                                SyntaxKind::Contextual => break InterpretMode::Code,
-                                SyntaxKind::Conditional => break InterpretMode::Code,
-                                SyntaxKind::WhileLoop => break InterpretMode::Code,
-                                SyntaxKind::ForLoop => break InterpretMode::Code,
-                                SyntaxKind::ModuleImport => break InterpretMode::Code,
-                                SyntaxKind::ImportItems => break InterpretMode::Code,
-                                SyntaxKind::RenamedImportItem => break InterpretMode::Code,
-                                SyntaxKind::ModuleInclude => break InterpretMode::Code,
-                                SyntaxKind::LoopBreak => break InterpretMode::Code,
-                                SyntaxKind::LoopContinue => break InterpretMode::Code,
-                                SyntaxKind::FuncReturn => break InterpretMode::Code,
-                                SyntaxKind::Destructuring => break InterpretMode::Code,
-                                SyntaxKind::DestructAssignment => break InterpretMode::Code,
-                                SyntaxKind::Error => {}
-                                SyntaxKind::Eof => {}
+                                LineComment | BlockComment => break InterpretMode::Comment,
+                                Raw => break InterpretMode::Raw,
+                                Str => break InterpretMode::String,
+                                CodeBlock | Code => break InterpretMode::Code,
+                                ContentBlock | Markup => break InterpretMode::Markup,
+                                Equation | Math => break InterpretMode::Math,
+                                Space | Linebreak | Parbreak | Escape | Shorthand | SmartQuote
+                                | RawLang | RawDelim | RawTrimmed | Hash | LeftBrace
+                                | RightBrace | LeftBracket | RightBracket | LeftParen
+                                | RightParen | Comma | Semicolon | Colon | Star | Underscore
+                                | Dollar | Plus | Minus | Slash | Hat | Prime | Dot | Eq | EqEq
+                                | ExclEq | Lt | LtEq | Gt | GtEq | PlusEq | HyphEq | StarEq
+                                | SlashEq | Dots | Arrow | Root | Not | And | Or | None | Auto
+                                | As | Named | Keyed | Error | Eof => {}
+                                Text | Strong | Emph | Link | Label | Ref | RefMarker | Heading
+                                | HeadingMarker | ListItem | ListMarker | EnumItem | EnumMarker
+                                | TermItem | TermMarker => break InterpretMode::Markup,
+                                MathIdent | MathAlignPoint | MathDelimited | MathAttach
+                                | MathPrimes | MathFrac | MathRoot => break InterpretMode::Math,
+                                Let | Set | Show | Context | If | Else | For | In | While
+                                | Break | Continue | Return | Import | Include | Ident | Bool
+                                | Int | Float | Numeric | FieldAccess | Args | Spread | Closure
+                                | Params | LetBinding | SetRule | ShowRule | Contextual
+                                | Conditional | WhileLoop | ForLoop | ModuleImport
+                                | ImportItems | RenamedImportItem | ModuleInclude | LoopBreak
+                                | LoopContinue | FuncReturn | FuncCall | Unary | Binary
+                                | Parenthesized | Dict | Array | Destructuring
+                                | DestructAssignment => break InterpretMode::Code,
                             }
                             leaf = t.parent();
                         } else {
