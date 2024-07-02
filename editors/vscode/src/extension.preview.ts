74fe2d7a7e6cb00f7376f089eb6d580d25caaf5f
Myriad-Dreamin <camiyoru@gmail.com>
2024-07-02 20:45:59 +0800

    fix: indent

--- editors/vscode/src/extension.preview.ts
+++ editors/vscode/src/extension.preview.ts
@@ -6,10 +6,10 @@ import { previewActive, previewDeactivate } from "./preview";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  return previewActive(context, true);
+    return previewActive(context, true);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {
-  return previewDeactivate();
+    return previewDeactivate();
 }
