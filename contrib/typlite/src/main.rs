f8c421853452301763d3cc5615a0841ebebeec1a
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2024-07-23 19:48:14 +0800

    feat: introduce typlite's cli (#451)
    * feat: introduce typlite's cli
    
    * build: specify default-bin
    
    * fix
    
    * fix: default bin again
    
    * dev: ignore typlite's lock

---
+++ contrib/typlite/src/main.rs
@@ -0,0 +1,13 @@
+//! # Typlite
+
+extern crate typlite;
+
+use typlite::Typlite;
+
+fn main() {
+    let input = r#"..\..\..\..\ts\typst-blog\source\_posts\simple-se.typ"#;
+    let input = std::fs::read_to_string(input).unwrap();
+    let typlite = Typlite::new_with_content(&input);
+    let output = typlite.convert().unwrap();
+    std::fs::write(r#"..\..\..\..\ts\typst-blog\public\simple-se.md"#, output).unwrap();
+}
