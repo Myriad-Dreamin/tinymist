a2cf951d2d344e95680e8dceed684cba2136f7b4
Myriad-Dreamin <camiyoru@gmail.com>
2024-07-30 14:33:16 +0800

    feat: parse sys arguments for typlite

--- contrib/typlite/src/main.rs
+++ contrib/typlite/src/main.rs
@@ -1,13 +1,21 @@
-//! # Typlite
+#![doc = include_str!("../README.md")]
 
 extern crate typlite;
+
+use std::path::{Path, PathBuf};
 
 use typlite::Typlite;
 
 fn main() {
-    let input = r#"..\..\..\..\ts\typst-blog\source\_posts\simple-se.typ"#;
+    let input = std::env::args().nth(1).unwrap();
+    let input = Path::new(&input);
+    let output = std::env::args()
+        .nth(2)
+        .map(PathBuf::from)
+        .or_else(|| Some(input.with_extension("md")))
+        .unwrap();
+
     let input = std::fs::read_to_string(input).unwrap();
     let typlite = Typlite::new_with_content(&input);
-    let output = typlite.convert().unwrap();
-    std::fs::write(r#"..\..\..\..\ts\typst-blog\public\simple-se.md"#, output).unwrap();
+    std::fs::write(output, typlite.convert().unwrap().as_str()).unwrap();
 }
