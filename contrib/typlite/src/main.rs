67e4bbf1b5afdf3eb4fc6b6182558fffe1757b79
Myriad-Dreamin <camiyoru@gmail.com>
2024-07-30 16:01:19 +0800

    dev: allow output to stdout

--- contrib/typlite/src/main.rs
+++ contrib/typlite/src/main.rs
@@ -9,18 +9,19 @@ use typlite::Typlite;
 fn main() {
     let input = std::env::args().nth(1).unwrap();
     let input = Path::new(&input);
-    let output = std::env::args()
-        .nth(2)
-        .map(PathBuf::from)
-        .or_else(|| Some(input.with_extension("md")))
-        .unwrap();
+    let output = match std::env::args().nth(2) {
+        Some(e) if e == "-" => None,
+        Some(e) => Some(PathBuf::from(e)),
+        None => Some(input.with_extension("md")),
+    };
 
     let input = std::fs::read_to_string(input).unwrap();
     let conv = Typlite::new_with_content(&input).convert();
 
-    match conv {
-        Ok(conv) => std::fs::write(output, conv.as_str()).unwrap(),
-        Err(e) => {
+    match (conv, output) {
+        (Ok(conv), None) => println!("{}", conv),
+        (Ok(conv), Some(output)) => std::fs::write(output, conv.as_str()).unwrap(),
+        (Err(e), ..) => {
             eprintln!("{e}");
             std::process::exit(1);
         }
