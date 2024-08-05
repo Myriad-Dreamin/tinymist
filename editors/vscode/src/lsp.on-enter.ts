3a36566bc3f2788784ff082494f357723f7d9866
Myriad-Dreamin <camiyoru@gmail.com>
2024-08-06 04:13:04 +0800

    refactor: dedent on enter handler

--- editors/vscode/src/lsp.on-enter.ts
+++ editors/vscode/src/lsp.on-enter.ts
@@ -9,37 +9,35 @@ const onEnter = new lc.RequestType<lc.TextDocumentPositionParams, lc.TextEdit[],
   "experimental/onEnter",
 );
 
-export function onEnterHandler() {
-  async function handleKeypress() {
-    if (!extensionState.features.onEnter) return false;
-
-    const editor = activeTypstEditor();
-
-    if (!editor || !client) return false;
-
-    const lcEdits = await client
-      .sendRequest(onEnter, {
-        textDocument: client.code2ProtocolConverter.asTextDocumentIdentifier(editor.document),
-        position: client.code2ProtocolConverter.asPosition(editor.selection.active),
-      })
-      .catch((_error: any) => {
-        // client.handleFailedRequest(OnEnterRequest.type, error, null);
-        return null;
-      });
-    if (!lcEdits) return false;
-
-    const edits = await client.protocol2CodeConverter.asTextEdits(lcEdits);
-    await applySnippetTextEdits(editor, edits);
-    return true;
+export async function onEnterHandler() {
+  try {
+    if (await handleKeypress()) return;
+  } catch (e) {
+    console.error("onEnter failed", e);
   }
 
-  return async () => {
-    try {
-      if (await handleKeypress()) return;
-    } catch (e) {
-      console.error("onEnter failed", e);
-    }
+  await vscode.commands.executeCommand("default:type", { text: "\n" });
+}
 
-    await vscode.commands.executeCommand("default:type", { text: "\n" });
-  };
+async function handleKeypress() {
+  if (!extensionState.features.onEnter) return false;
+
+  const editor = activeTypstEditor();
+
+  if (!editor || !client) return false;
+
+  const lcEdits = await client
+    .sendRequest(onEnter, {
+      textDocument: client.code2ProtocolConverter.asTextDocumentIdentifier(editor.document),
+      position: client.code2ProtocolConverter.asPosition(editor.selection.active),
+    })
+    .catch((_error: any) => {
+      // client.handleFailedRequest(OnEnterRequest.type, error, null);
+      return null;
+    });
+  if (!lcEdits) return false;
+
+  const edits = await client.protocol2CodeConverter.asTextEdits(lcEdits);
+  await applySnippetTextEdits(editor, edits);
+  return true;
 }
