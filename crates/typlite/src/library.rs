a53b9d92d796fd322eef2c6229d88b7e930f9840
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2024-08-26 21:41:09 +0800

    docs: add notes to stateful pin commands (#562)

--- crates/typlite/src/library.rs
+++ crates/typlite/src/library.rs
@@ -11,6 +11,11 @@ pub fn library() -> Scopes<Value> {
     scopes.define("md-alter", md_alter as RawFunc);
     scopes.define("image", image as RawFunc);
     scopes.define("figure", figure as RawFunc);
+    scopes.define("note-box", note as RawFunc);
+    scopes.define("tip-box", tip as RawFunc);
+    scopes.define("important-box", important_box as RawFunc);
+    scopes.define("warning-box", warning_box as RawFunc);
+    scopes.define("caution-box", caution_box as RawFunc);
     scopes
 }
 
@@ -73,4 +78,54 @@ pub fn md_alter(mut args: Args) -> Result<Value> {
     let right = get_pos_named!(args, right: LazyContent);
 
     Ok(Value::Content(right.0))
+}
+
+/// Evaluate a note.
+pub fn note(mut args: Args) -> Result<Value> {
+    let body = get_pos_named!(args, body: Content);
+
+    Ok(note_box("NOTE", body))
+}
+
+/// Evaluate a tip note box.
+pub fn tip(mut args: Args) -> Result<Value> {
+    let body = get_pos_named!(args, body: Content);
+
+    Ok(note_box("TIP", body))
+}
+
+/// Create a important note box.
+pub fn important_box(mut args: Args) -> Result<Value> {
+    let body = get_pos_named!(args, body: Content);
+
+    Ok(note_box("IMPORTANT", body))
+}
+
+/// Create a warning note box.
+pub fn warning_box(mut args: Args) -> Result<Value> {
+    let body = get_pos_named!(args, body: Content);
+
+    Ok(note_box("WARNING", body))
+}
+
+/// Create a caution note box.
+pub fn caution_box(mut args: Args) -> Result<Value> {
+    let body = get_pos_named!(args, body: Content);
+
+    Ok(note_box("CAUTION", body))
+}
+
+fn note_box(title: &str, body: Content) -> Value {
+    let mut res = EcoString::new();
+    res.push_str("> [!");
+    res.push_str(title);
+    res.push_str("]\n");
+    let body = body.0;
+    for line in body.lines() {
+        res.push_str("> ");
+        res.push_str(line);
+        res.push('\n');
+    }
+
+    Value::Content(res)
 }
