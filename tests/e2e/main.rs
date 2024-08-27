5c57d54f17aad0104a1eb7c4871ba8b4d37a68d4
Myriad-Dreamin <camiyoru@gmail.com>
2024-08-27 09:31:14 +0800

    fix: snapshot

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -374,7 +374,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("neovim"));
-        insta::assert_snapshot!(hash, @"siphash128_13:ec80ef1b6dd5a8ce4a22021a47683fc6");
+        insta::assert_snapshot!(hash, @"siphash128_13:9c366f2a8473a39d271c50dd8079a6e4");
     }
 
     {
@@ -385,7 +385,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:16e3c6c677e354093d0ab458a749329d");
+        insta::assert_snapshot!(hash, @"siphash128_13:5e46fecd62a56ad654d933738ef4b753");
     }
 }
 
