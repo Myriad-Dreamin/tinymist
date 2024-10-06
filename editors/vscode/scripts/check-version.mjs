268f2d536737cc762aed9ad34511f65afe35fee5
Myriad-Dreamin <camiyoru@gmail.com>
2024-10-06 21:02:15 +0800

    fix: failed version checking when cross compiling

--- editors/vscode/scripts/check-version.mjs
+++ editors/vscode/scripts/check-version.mjs
@@ -1,18 +1,14 @@
 
-import { execSync } from 'child_process';
-import { resolve } from 'path';
 import { readFileSync } from 'fs';
 
-const tinymistExecutable = resolve('out/tinymist' + (process.platform === 'win32' ? '.exe' : ''));
 
 function check() {
-    const version = JSON.parse(readFileSync('package.json', 'utf8')).version;
-    const expected = `tinymist ${version}`;
+    const cargoToml = readFileSync('../../Cargo.toml', 'utf8');
+    const cargoVersion = cargoToml.match(/version = "(.*?)"/)[1];
+    const pkgVersion = JSON.parse(readFileSync('package.json', 'utf8')).version;
 
-    const output = execSync(`${tinymistExecutable} -V`).toString().trim();
-
-    if (output !== expected) {
-        throw new Error(`Version mismatch: ${output} !== ${expected} (in package.json)`);
+    if (cargoVersion !== pkgVersion) {
+        throw new Error(`Version mismatch: ${cargoVersion} (in Cargo.toml) !== ${pkgVersion} (in package.json)`);
     }
 }
 
