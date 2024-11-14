a8e54628f56a04588df78eaf25c3ecdd7d295338
Myriad-Dreamin <camiyoru@gmail.com>
2024-11-14 22:07:57 +0800

    feat: fully support `onEnter` edits inside comments - select with range - multiple cursor

--- crates/tinymist-query/src/on_enter.rs
+++ crates/tinymist-query/src/on_enter.rs
@@ -20,8 +20,8 @@ use crate::{prelude::*, syntax::node_ancestors, SyntaxRequest};
 pub struct OnEnterRequest {
     /// The path of the document to get folding ranges for.
     pub path: PathBuf,
-    /// The source code position to request for.
-    pub position: LspPosition,
+    /// The source code range to request for.
+    pub range: LspRange,
 }
 
 impl SyntaxRequest for OnEnterRequest {
@@ -33,7 +33,8 @@ impl SyntaxRequest for OnEnterRequest {
         position_encoding: PositionEncoding,
     ) -> Option<Self::Response> {
         let root = LinkedNode::new(source.root());
-        let cursor = lsp_to_typst::position(self.position, position_encoding, source)?;
+        let rng = lsp_to_typst::range(self.range, position_encoding, source)?;
+        let cursor = rng.end;
         let leaf = root.leaf_at_compat(cursor)?;
 
         let worker = OnEnterWorker {
@@ -42,13 +43,13 @@ impl SyntaxRequest for OnEnterRequest {
         };
 
         if matches!(leaf.kind(), SyntaxKind::LineComment) {
-            return worker.enter_line_doc_comment(&leaf, cursor);
+            return worker.enter_line_doc_comment(&leaf, rng);
         }
 
         let math_node =
             node_ancestors(&leaf).find(|node| matches!(node.kind(), SyntaxKind::Equation));
         if let Some(mn) = math_node {
-            return worker.enter_block_math(mn, cursor);
+            return worker.enter_block_math(mn, rng);
         }
 
         None
@@ -68,7 +69,11 @@ impl OnEnterWorker<'_> {
         " ".repeat(indent_size)
     }
 
-    fn enter_line_doc_comment(&self, leaf: &LinkedNode, cursor: usize) -> Option<Vec<TextEdit>> {
+    fn enter_line_doc_comment(
+        &self,
+        leaf: &LinkedNode,
+        rng: Range<usize>,
+    ) -> Option<Vec<TextEdit>> {
         let skipper = |n: &LinkedNode| {
             matches!(
                 n.kind(),
@@ -100,8 +105,6 @@ impl OnEnterWorker<'_> {
         let indent = self.indent_of(leaf.offset());
         // todo: remove_trailing_whitespace
 
-        let rng = cursor..cursor;
-
         let edit = TextEdit {
             range: typst_to_lsp::range(rng, self.source, self.position_encoding),
             new_text: format!("\n{indent}{comment_prefix} $0"),
@@ -110,9 +113,13 @@ impl OnEnterWorker<'_> {
         Some(vec![edit])
     }
 
-    fn enter_block_math(&self, math_node: &LinkedNode<'_>, cursor: usize) -> Option<Vec<TextEdit>> {
+    fn enter_block_math(
+        &self,
+        math_node: &LinkedNode<'_>,
+        rng: Range<usize>,
+    ) -> Option<Vec<TextEdit>> {
         let o = math_node.range();
-        if !o.contains(&cursor) {
+        if !o.contains(&rng.end) {
             return None;
         }
 
@@ -124,7 +131,6 @@ impl OnEnterWorker<'_> {
         }
 
         let indent = self.indent_of(o.start);
-        let rng = cursor..cursor;
         let edit = TextEdit {
             range: typst_to_lsp::range(rng, self.source, self.position_encoding),
             // todo: read indent configuration
