8afc1216543e88933e70e0edf5c062d62060769c
Myriad-Dreamin <camiyoru@gmail.com>
2024-11-15 11:56:20 +0800

    fix: config

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -72,7 +72,7 @@ export async function doActivate(context: ExtensionContext): Promise<void> {
   config.triggerSuggest = true;
   config.triggerNamedCompletion = true;
   config.triggerParameterHints = true;
-  config.supportHtmlInMarkdown = false;
+  config.supportHtmlInMarkdown = true;
   // Sets features
   extensionState.features.preview = config.previewFeature === "enable";
   extensionState.features.devKit = isDevMode || config.devKit === "enable";
