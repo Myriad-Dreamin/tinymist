0edaed23014aea91f64571f7ef849dab52b89270
Yifan Song <Eric_YF_Song@outlook.com>
2024-12-12 07:12:44 +0100

    chore: add postfix for in

--- crates/tinymist-query/src/completion/snippet.rs
+++ crates/tinymist-query/src/completion/snippet.rs
@@ -635,5 +635,14 @@ pub static DEFAULT_POSTFIX_SNIPPET: LazyLock<Vec<PostfixSnippet>> = LazyLock::ne
             description: "wrap as let expression".into(),
             parsed_snippet: OnceLock::new(),
         },
+        PostfixSnippet {
+            scope: PostfixSnippetScope::Value,
+            mode: eco_vec![InterpretMode::Code, InterpretMode::Markup],
+            label: "in".into(),
+            label_detail: Some(".in".into()),
+            snippet: "${_} in ${node}".into(),
+            description: "wrap with in expression".into(),
+            parsed_snippet: OnceLock::new(),
+        },
     ]
 });
