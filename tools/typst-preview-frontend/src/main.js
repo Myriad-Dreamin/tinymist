adcc9e8a86a6c61d5e7e5f42f8afc163b30ed5bf
Myriad-Dreamin <camiyoru@gmail.com>
2024-12-12 23:53:42 +0800

    dev: add some comment

--- tools/typst-preview-frontend/src/main.js
+++ tools/typst-preview-frontend/src/main.js
@@ -37,8 +37,11 @@ function retrieveWsArgs() {
 
     /// The string `ws://127.0.0.1:23625` is a placeholder
     /// Also, it is the default url to connect to.
+    /// Note that we must resolve the url to an absolute url as
+    /// the websocket connection requires an absolute url.
+    ///
+    /// See [WebSocket and relative URLs](https://github.com/whatwg/websockets/issues/20)
     let urlObject = new URL("ws://127.0.0.1:23625", window.location.href);
-
     /// Rewrite the protocol to websocket.
     urlObject.protocol = urlObject.protocol.replace(/^https$/, 'wss').replace(/^http$/, 'ws');
     if (location.href.startsWith("https://")) {
