dafa72892493dad4be4cda3244edb08613108b6f
Myriad-Dreamin <camiyoru@gmail.com>
2025-01-07 12:09:20 +0800

    feat: add scripts to test syntax highlight (cont.)

--- syntaxes/textmate/scripts/make-bundle.cjs
+++ syntaxes/textmate/scripts/make-bundle.cjs
@@ -4,10 +4,20 @@ const fs = require("fs").promises;
 const path = require("path");
 
 /**
+ * @typedef {{
+ *   filePath: string;
+ *   content: string;
+ *   lineCount: number;
+ * }} SourceFile
+ *
  * @param {string} dirPath
+ * @returns {Promise<SourceFile[]>}
  */
-async function makeBundle(dirPath) {
+async function readFiles(dirPath) {
   // Scans all .typ files recursively
+  /**
+   * @returns {Promise<SourceFile[]>}
+   */
   const scanTypFiles = async (dirPath) => {
     const files = await fs.readdir(dirPath, { withFileTypes: true });
     const typFiles = [];
@@ -20,8 +30,9 @@ async function makeBundle(dirPath) {
         } else if (file.isFile() && file.name.endsWith(".typ")) {
           // push filename
           const filePath = path.join(dirPath, file.name).replace(/\\/g, "/");
-          typFiles.push(`// ${filePath}`);
-          typFiles.push(await fs.readFile(filePath, "utf8"));
+          const content = await fs.readFile(filePath, "utf8");
+          const lineCount = content.split("\n").length;
+          typFiles.push({ filePath, content, lineCount });
         }
       })
     );
@@ -29,28 +40,63 @@ async function makeBundle(dirPath) {
     return typFiles;
   };
 
-  const typFiles = await scanTypFiles(dirPath);
-  const bundle = typFiles.join("\n");
-  return bundle;
+  const files = await scanTypFiles(dirPath);
+  // sort by file name
+  files.sort((a, b) => {
+    return a.filePath.localeCompare(b.filePath);
+  });
+
+  return files;
+}
+
+async function writeBundle(bundles, outPath) {
+  // split bundle per 100000 lines
+  const chunked = [];
+  let chunk = [];
+  let totalLineCount = 0;
+  for (const file of bundles) {
+    if (totalLineCount + file.lineCount > 100000) {
+      chunked.push(chunk);
+      chunk = [];
+      totalLineCount = 0;
+    }
+    chunk.push(`// ${file.filePath}`);
+    chunk.push(file.content);
+    totalLineCount += file.lineCount;
+  }
+  if (chunk.length > 0) {
+    chunked.push(chunk);
+  }
+
+  // write bundle
+  await Promise.all(
+    chunked.map(async (chunk, index) => {
+      const chunkPath = outPath.replace(".typ", `-${index}.typ`);
+      await fs.writeFile(chunkPath, chunk.join("\n")).then(() => {
+        console.log(`Wrote ${chunkPath}`);
+      });
+    })
+  );
 }
 
 async function main() {
   await fs.mkdir(path.join(__dirname, "../tests/bundles"), { recursive: true });
 
   {
-    const bundle = await makeBundle(path.join(__dirname, "../tests/packages"));
+    // const bundle = typFiles.join("\n");
+    const bundles = await readFiles(path.join(__dirname, "../tests/packages"));
     const outPath = path.join(__dirname, "../tests/bundles/typst-packages.typ");
-    await fs.writeFile(outPath, bundle);
+    await writeBundle(bundles, outPath);
   }
   {
-    const bundle = await makeBundle(
+    const bundles = await readFiles(
       path.join(__dirname, "../tests/official-testing")
     );
     const outPath = path.join(
       __dirname,
       "../tests/bundles/typst-official-testing.typ"
     );
-    await fs.writeFile(outPath, bundle);
+    await writeBundle(bundles, outPath);
   }
 }
 
