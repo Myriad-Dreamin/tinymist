27d6cc74b81d597096b4ae42d734ba6e2a5114b4
Myriad-Dreamin <camiyoru@gmail.com>
2025-01-14 13:35:26 +0800

    docs: add comments

--- crates/crityp/src/lib.rs
+++ crates/crityp/src/lib.rs
@@ -1,3 +1,15 @@
+//! Crityp is a crate for benchmarking typst code.
+//!
+//! ## Usage
+//!
+//! ```rs
+//! let mut crit = criterion::Criterion::default();
+//! let mut verse = tinymist_world::CompileOnceArgs::parse().resolve()?;
+//! let mut world = verse.snapshot();
+//! crityp::bench(&mut crit, &mut world)?;
+//! crit.final_summary();
+//! ```
+
 use anyhow::Context as ContextTrait;
 use comemo::Track;
 use criterion::Criterion;
@@ -9,7 +21,10 @@ use typst::foundations::{Context, Func, Value};
 use typst::introspection::Introspector;
 use typst::World;
 
+/// Runs benchmarks on the given world. An entry point must be provided in the
+/// world.
 pub fn bench(c: &mut Criterion, world: &mut LspWorld) -> anyhow::Result<()> {
+    // Gets the main source file and its path.
     let main_source = world.source(world.main())?;
     let main_path = unix_slash(world.main().vpath().as_rooted_path());
 
@@ -18,6 +33,7 @@ pub fn bench(c: &mut Criterion, world: &mut LspWorld) -> anyhow::Result<()> {
     let traced = Traced::default();
     let introspector = Introspector::default();
 
+    // Evaluates the main source file.
     let module = typst::eval::eval(
         ((world) as &dyn World).track(),
         traced.track(),
@@ -29,6 +45,7 @@ pub fn bench(c: &mut Criterion, world: &mut LspWorld) -> anyhow::Result<()> {
         .map_err(|e| anyhow::anyhow!("{e:?}"))
         .context("evaluation error")?;
 
+    // Collects all benchmarks.
     let mut goals: Vec<(EcoString, &Func)> = vec![];
     for (name, value, _) in module.scope().iter() {
         if !name.starts_with("bench") {
@@ -40,6 +57,7 @@ pub fn bench(c: &mut Criterion, world: &mut LspWorld) -> anyhow::Result<()> {
         }
     }
 
+    // Runs benchmarks.
     for (name, func) in goals {
         let route = Route::default();
         let mut sink = Sink::default();
@@ -51,17 +69,22 @@ pub fn bench(c: &mut Criterion, world: &mut LspWorld) -> anyhow::Result<()> {
             route,
         };
 
+        // Runs the benchmark once.
         let mut call_once = move || {
             let context = Context::default();
             let values = Vec::<Value>::default();
             func.call(engine, context.track(), values)
         };
 
+        // Calls the benchmark once to ensure it is correct.
+        // Since all typst functions are pure, we can safely ignore the result
+        // in the benchmark loop then.
         if let Err(err) = call_once() {
             eprintln!("call error in {name}: {err:?}");
             continue;
         }
 
+        // Benchmarks the function
         c.bench_function(&name, move |b| {
             b.iter(|| {
                 comemo::evict(0);
