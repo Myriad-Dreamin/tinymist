51e795f76f9d06126cff481ab905f17286d68034
Myriad-Dreamin <camiyoru@gmail.com>
2025-01-28 14:03:56 +0800

    dev: update build script sample

--- scripts/project-build.ps1
+++ scripts/project-build.ps1
@@ -1,6 +1,6 @@
 # From file:docs/tinymist/book.typ to file:docs/tinymist/book.typ (pdf)
-cargo run --bin tinymist -- compile --save-lock --task 'file:docs/tinymist/book.typ' 'docs/tinymist/book.typ' --root '.' --format=pdf
+tinymist compile --save-lock --task 'file:docs/tinymist/book.typ' 'docs/tinymist/book.typ' --root '.' --format=pdf
 # From file:docs/tinymist/ebook.typ to ebook-cover (svg)
-cargo run --bin tinymist -- compile --save-lock --task 'ebook-cover' 'docs/tinymist/ebook.typ' --root '.' --pages 1-1 --format=svg
+tinymist compile --save-lock --task 'ebook-cover' 'docs/tinymist/ebook.typ' --root '.' --pages 1-1 --format=svg
 # From file:docs/tinymist/ebook.typ to file:docs/tinymist/ebook.typ (pdf)
-cargo run --bin tinymist -- compile --save-lock --task 'file:docs/tinymist/ebook.typ' 'docs/tinymist/ebook.typ' --root '.' --format=pdf
+tinymist compile --save-lock --task 'file:docs/tinymist/ebook.typ' 'docs/tinymist/ebook.typ' --root '.' --format=pdf
