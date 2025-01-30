87fb7d819bde37e52b8dd48a82b64221fa95495d
Myriad-Dreamin <camiyoru@gmail.com>
2025-01-30 20:25:42 +0800

    refactor: rearrange state methods

--- crates/tinymist/src/state/input.rs
+++ crates/tinymist/src/state/input.rs
@@ -1,145 +1,17 @@
 use std::path::PathBuf;
 
-use lsp_types::request::WorkspaceConfiguration;
 use lsp_types::*;
-use once_cell::sync::OnceCell;
 use reflexo_typst::Bytes;
-use serde_json::{Map, Value as JsonValue};
-use sync_lsp::*;
 use tinymist_project::{Interrupt, ProjectResolutionKind};
 use tinymist_query::{to_typst_range, PositionEncoding};
-use tinymist_std::error::{prelude::*, IgnoreLogging};
+use tinymist_std::error::prelude::*;
 use tinymist_std::ImmutPath;
 use typst::{diag::FileResult, syntax::Source};
 
 use crate::route::ProjectResolution;
-use crate::task::FormatterConfig;
 use crate::world::vfs::{notify::MemoryEvent, FileChangeSet};
 use crate::world::TaskInputs;
-use crate::{init::*, *};
-
-/// LSP Document Synchronization
-impl ServerState {
-    pub(crate) fn did_open(&mut self, params: DidOpenTextDocumentParams) -> LspResult<()> {
-        log::info!("did open {:?}", params.text_document.uri);
-        let path = as_path_(params.text_document.uri);
-        let text = params.text_document.text;
-
-        self.create_source(path.clone(), text)
-            .map_err(|e| invalid_params(e.to_string()))?;
-
-        // Focus after opening
-        self.implicit_focus_entry(|| Some(path.as_path().into()), 'o');
-        Ok(())
-    }
-
-    pub(crate) fn did_close(&mut self, params: DidCloseTextDocumentParams) -> LspResult<()> {
-        let path = as_path_(params.text_document.uri);
-
-        self.remove_source(path.clone())
-            .map_err(|e| invalid_params(e.to_string()))?;
-        Ok(())
-    }
-
-    pub(crate) fn did_change(&mut self, params: DidChangeTextDocumentParams) -> LspResult<()> {
-        let path = as_path_(params.text_document.uri);
-        let changes = params.content_changes;
-
-        self.edit_source(path.clone(), changes, self.const_config().position_encoding)
-            .map_err(|e| invalid_params(e.to_string()))?;
-        Ok(())
-    }
-
-    pub(crate) fn did_save(&mut self, _params: DidSaveTextDocumentParams) -> LspResult<()> {
-        Ok(())
-    }
-}
-
-/// LSP Configuration Synchronization
-impl ServerState {
-    pub(crate) fn on_changed_configuration(
-        &mut self,
-        values: Map<String, JsonValue>,
-    ) -> LspResult<()> {
-        let old_config = self.config.clone();
-        match self.config.update_by_map(&values) {
-            Ok(()) => {}
-            Err(err) => {
-                self.config = old_config;
-                log::error!("error applying new settings: {err}");
-                return Err(invalid_params(format!(
-                    "error applying new settings: {err}"
-                )));
-            }
-        }
-
-        let new_export_config = self.config.export();
-        if old_config.export() != new_export_config {
-            self.change_export_config(new_export_config);
-        }
-
-        if old_config.compile.primary_opts() != self.config.compile.primary_opts() {
-            self.config.compile.fonts = OnceCell::new(); // todo: don't reload fonts if not changed
-            self.restart_primary()
-                .log_error("could not restart primary");
-        }
-
-        if old_config.semantic_tokens != self.config.semantic_tokens {
-            self.enable_sema_token_caps(self.config.semantic_tokens == SemanticTokensMode::Enable)
-                .log_error("could not change semantic tokens config");
-        }
-
-        let new_formatter_config = self.config.formatter();
-        if !old_config.formatter().eq(&new_formatter_config) {
-            let enabled = !matches!(new_formatter_config.config, FormatterConfig::Disable);
-            self.enable_formatter_caps(enabled)
-                .log_error("could not change formatter config");
-
-            self.formatter.change_config(new_formatter_config);
-        }
-
-        log::info!("new settings applied");
-        Ok(())
-    }
-
-    pub(crate) fn did_change_configuration(
-        &mut self,
-        params: DidChangeConfigurationParams,
-    ) -> LspResult<()> {
-        // For some clients, we don't get the actual changed configuration and need to
-        // poll for it https://github.com/microsoft/language-server-protocol/issues/676
-        if let JsonValue::Object(settings) = params.settings {
-            return self.on_changed_configuration(settings);
-        };
-
-        self.client.send_request::<WorkspaceConfiguration>(
-            ConfigurationParams {
-                items: Config::get_items(),
-            },
-            Self::workspace_configuration_callback,
-        );
-        Ok(())
-    }
-
-    fn workspace_configuration_callback(this: &mut ServerState, resp: lsp_server::Response) {
-        if let Some(err) = resp.error {
-            log::error!("failed to request configuration: {err:?}");
-            return;
-        }
-
-        let Some(result) = resp.result else {
-            log::error!("no configuration returned");
-            return;
-        };
-
-        let Some(resp) = serde_json::from_value::<Vec<JsonValue>>(result)
-            .log_error("could not parse configuration")
-        else {
-            return;
-        };
-        let _ = this.on_changed_configuration(Config::values_to_map(resp));
-    }
-}
+use crate::*;
 
 /// In memory source file management.
 impl ServerState {
