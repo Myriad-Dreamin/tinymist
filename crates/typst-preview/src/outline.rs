b746520a8076a067124bf5a7821772f60f6ae4f9
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-02 19:11:41 +0800

    fix: error

--- crates/typst-preview/src/outline.rs
+++ crates/typst-preview/src/outline.rs
@@ -1,8 +1,8 @@
 use std::num::NonZeroUsize;
 
 use reflexo_typst::debug_loc::DocumentPosition;
-use reflexo_typst::TypstDocument;
 use serde::{Deserialize, Serialize};
+use tinymist_std::typst::TypstDocument;
 use typst::foundations::{Content, NativeElement, Packed, StyleChain};
 use typst::introspection::Introspector;
 use typst::model::HeadingElem;
