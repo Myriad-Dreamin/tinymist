76c7c8588882b4e0a56646ff0aa29ba197e6829e
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-02 18:45:24 +0800

    test: update snapshots

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -374,7 +374,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("neovim"));
-        insta::assert_snapshot!(hash, @"siphash128_13:b3c34687da7a40cef9adc4f56a1f4ba8");
+        insta::assert_snapshot!(hash, @"siphash128_13:f7839d55d1d4550782503faa089cfcf8");
     }
 
     {
@@ -385,7 +385,7 @@ fn e2e() {
         });
 
         let hash = replay_log(&tinymist_binary, &root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:28c1e597e2082585f5042d8ca3eb1a88");
+        insta::assert_snapshot!(hash, @"siphash128_13:6459f45eedb812b0ab987fe2208d40ea");
     }
 }
 
