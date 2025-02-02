f9fc0e4872d754fe047ff4da32260fb1b3c481b8
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-02 19:11:50 +0800

    html changes

--- crates/tinymist-world/src/tests.rs
+++ crates/tinymist-world/src/tests.rs
@@ -5,13 +5,11 @@ use crate::args::CompileOnceArgs;
 #[test]
 #[cfg(feature = "system")]
 fn test_args() {
-    use tinymist_std::typst::TypstPagedDocument;
-
     let args = CompileOnceArgs::parse_from(["tinymist", "main.typ"]);
     let verse = args
         .resolve_system()
         .expect("failed to resolve system universe");
 
     let world = verse.snapshot();
-    let _res = typst::compile::<TypstPagedDocument>(&world);
+    let _res = typst::compile(&world);
 }
