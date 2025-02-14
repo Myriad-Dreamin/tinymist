148e5ba1f98a537f393cc692a748df7df4fd3a6c
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-14 15:44:18 +0800

    feat: add FromArc and ArcInto

--- crates/tinymist-std/src/concepts/mod.rs
+++ crates/tinymist-std/src/concepts/mod.rs
@@ -23,3 +23,33 @@ pub type ImmutStr = Arc<str>;
 pub type ImmutBytes = Arc<[u8]>;
 /// An immutable path.
 pub type ImmutPath = Arc<Path>;
+
+/// A trait for converting an `Arc<T>` into `Self`.
+pub trait FromArc<T> {
+    /// Converts an `Arc<T>` into `Self`.
+    fn from_arc(arc: Arc<T>) -> Self;
+}
+
+impl<S, T> FromArc<S> for T
+where
+    Arc<S>: Into<T>,
+{
+    fn from_arc(arc: Arc<S>) -> T {
+        arc.into()
+    }
+}
+
+/// A trait for converting `Arc<T>` into `Self`.
+pub trait ArcInto<T> {
+    /// Converts `Arc<T>` into `Self`.
+    fn arc_into(self: Arc<Self>) -> T;
+}
+
+impl<S, T> ArcInto<T> for S
+where
+    Arc<S>: Into<T>,
+{
+    fn arc_into(self: Arc<Self>) -> T {
+        self.into()
+    }
+}
