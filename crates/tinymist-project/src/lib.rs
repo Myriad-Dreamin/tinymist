80b8b67cd24f1e2c26e9a94349fd3c837441a925
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-16 10:03:16 +0800

    fix: feature gate 3

--- crates/tinymist-project/src/lib.rs
+++ crates/tinymist-project/src/lib.rs
@@ -3,19 +3,24 @@
 mod args;
 mod compiler;
 mod entry;
+mod model;
+
 #[cfg(feature = "system")]
 pub mod font;
+#[cfg(feature = "system")]
 mod lock;
-mod model;
 #[cfg(feature = "system")]
 mod watch;
 #[cfg(feature = "system")]
 pub mod world;
+
 pub use args::*;
 pub use compiler::*;
 pub use entry::*;
-pub use lock::*;
 pub use model::*;
+
+#[cfg(feature = "system")]
+pub use lock::*;
 #[cfg(feature = "system")]
 pub use watch::*;
 #[cfg(feature = "system")]
