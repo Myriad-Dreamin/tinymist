2853af95c0f2b47d3358902d75ccd783af2d9f01
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-24 11:59:23 +0800

    docs: add some comment

--- editors/vscode/src/lsp.ts
+++ editors/vscode/src/lsp.ts
@@ -542,6 +542,10 @@ export class LanguageState {
       ];
     }
 
+    // This is different from `wordSeparators`. We need to document difference here.
+    // todo: document difference here.
+    //
+    // https://code.visualstudio.com/api/language-extensions/language-configuration-guide#word-pattern
     const wordPattern =
       /(-?\d*.\d\w*)|([^\`\~\!\@\#\$\%\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.<\>\/\?\s]+)/;
 
