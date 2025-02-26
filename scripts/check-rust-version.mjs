63ab7614c8dfe73f59b7d7c06522c104465b323e
Myriad-Dreamin <camiyoru@gmail.com>
2025-02-26 13:51:07 +0800

    test: check toolchain

--- scripts/check-rust-version.mjs
+++ scripts/check-rust-version.mjs
@@ -2,21 +2,26 @@ import fs from "fs";
 
 const ciFile = fs.readFileSync(".github/workflows/release-vscode.yml", "utf-8");
 
-const checkMinVersion = ciFile.match(
-  /# region: check-min-version([\s\S]+?)# end-region: check-min-version/gm,
-);
-if (!checkMinVersion) {
+const toolchainRe = (forWhat) =>
+  new RegExp(/dtolnay\/rust-toolchain@(\d+\.\d+\.\d+)/.source + `\\s*#\\s*${forWhat}`);
+
+const ciCheckedVersion = ciFile.match(toolchainRe("check-min-version"))?.[1];
+if (!ciCheckedVersion) {
   console.error("Check minimum version not found");
   process.exit(1);
 }
 
-const toolchainRe = /dtolnay\/rust-toolchain@(\d+\.\d+\.\d+)/g;
-
-const ciCheck = checkMinVersion[0];
-const ciCheckedVersion = ciCheck.match(toolchainRe)[1];
+const ciBuildVersion = ciFile.match(toolchainRe("build-extension-version"))?.[1];
+if (!ciBuildVersion) {
+  console.error("Build extension version not found");
+  process.exit(1);
+}
 
 const cargoToml = fs.readFileSync("Cargo.toml", "utf-8");
 const cargoSpecifiedVersion = cargoToml.match(/rust-version = "(\d+\.\d+)"/)[1];
+
+const toolchainToml = fs.readFileSync("rust-toolchain.toml", "utf-8");
+const toolchainSpecifiedVersion = toolchainToml.match(/channel = "(\d+\.\d+)"/)[1];
 
 function parseVersion(version) {
   const versions = version.split(".").map(Number);
@@ -41,9 +46,26 @@ function versionLess(a, b) {
 
 const specified = parseVersion(cargoSpecifiedVersion);
 const checked = parseVersion(ciCheckedVersion);
+const build = parseVersion(ciBuildVersion);
+const toolchain = parseVersion(toolchainSpecifiedVersion);
+
 if (versionLess(specified, checked)) {
   console.error(
     `Specified version ${cargoSpecifiedVersion} is less than checked version ${ciCheckedVersion}`,
+  );
+  process.exit(1);
+}
+
+if (versionLess(build, toolchain) || versionLess(toolchain, build)) {
+  console.error(
+    `Build version ${ciBuildVersion} does not match toolchain version ${toolchainSpecifiedVersion}`,
+  );
+  process.exit(1);
+}
+
+if (versionLess(toolchain, checked)) {
+  console.error(
+    `Toolchain version ${toolchainSpecifiedVersion} is less than checked version ${ciCheckedVersion}`,
   );
   process.exit(1);
 }
