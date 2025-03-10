20746e3acd9320825aae9a9076d35c8cbb3a27c1
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2025-03-10 17:42:23 +0800

    feat: improve `ArgSlots` a bit (#1479)

--- tools/editor-tools/src/features/summary.ts
+++ tools/editor-tools/src/features/summary.ts
@@ -1,16 +1,11 @@
 import van, { ChildDom, State } from "vanjs-core";
 import { stringify as csvStringify } from "csv-stringify/browser/esm/sync";
-import {
-  requestRevealPath,
-  requestSaveFontsExportConfigure,
-  saveDataToFile,
-} from "../vscode";
+import { requestRevealPath, requestSaveFontsExportConfigure, saveDataToFile } from "../vscode";
 import { CopyIcon } from "../icons";
 import { startModal } from "../components/modal";
 import { base64Decode, base64Encode } from "../utils";
 import { FontSource, humanStretch, humanStyle, humanWeight } from "../types";
-const { div, a, span, code, br, button, form, textarea, label, input } =
-  van.tags;
+const { div, a, span, code, br, button, form, textarea, label, input } = van.tags;
 
 interface ServerInfo {
   root: string;
@@ -24,16 +19,12 @@ type ServerInfoMap = Record<string, ServerInfo>;
 export const Summary = () => {
   const documentMetricsData = `:[[preview:DocumentMetrics]]:`;
   const docMetrics = van.state<DocumentMetrics>(
-    documentMetricsData.startsWith(":")
-      ? DOC_MOCK
-      : JSON.parse(base64Decode(documentMetricsData))
+    documentMetricsData.startsWith(":") ? DOC_MOCK : JSON.parse(base64Decode(documentMetricsData)),
   );
   console.log("docMetrics", docMetrics);
   const serverInfoData = `:[[preview:ServerInfo]]:`;
   const serverInfos = van.state<ServerInfoMap>(
-    serverInfoData.startsWith(":")
-      ? SERVER_INFO_MOCK
-      : JSON.parse(base64Decode(serverInfoData))
+    serverInfoData.startsWith(":") ? SERVER_INFO_MOCK : JSON.parse(base64Decode(serverInfoData)),
   );
   console.log("serverInfos", serverInfos);
 
@@ -46,8 +37,7 @@ export const Summary = () => {
         title = w.path;
         fontName = a(
           {
-            style:
-              "font-size: 1.2em; text-decoration: underline; cursor: pointer;",
+            style: "font-size: 1.2em; text-decoration: underline; cursor: pointer;",
             title,
             onclick() {
               if (w.kind === "fs") {
@@ -55,7 +45,7 @@ export const Summary = () => {
               }
             },
           },
-          font.name
+          font.name,
         );
       } else {
         title = `Embedded: ${w.name}`;
@@ -64,7 +54,7 @@ export const Summary = () => {
             style: "font-size: 1.2em",
             title,
           },
-          font.name
+          font.name,
         );
       }
     } else {
@@ -81,49 +71,45 @@ export const Summary = () => {
       code("Variant"),
       ": ",
       code(
-        font.style === "normal" || !font.style
-          ? ""
-          : `${humanStyle(font.style)}, `,
-        span(
-          { title: `Weight ${font.weight || 400}` },
-          `${humanWeight(font.weight)} Weight`
-        ),
+        font.style === "normal" || !font.style ? "" : `${humanStyle(font.style)}, `,
+        span({ title: `Weight ${font.weight || 400}` }, `${humanWeight(font.weight)} Weight`),
         ", ",
         span(
           { title: `Stretch ${(font.stretch || 1000) / 10}%` },
-          `${humanStretch(font.stretch)} Stretch`
-        )
+          `${humanStretch(font.stretch)} Stretch`,
+        ),
       ),
       br(),
       code("PostScriptName"),
       ": ",
       code(font.postscriptName),
       br(),
-      code(
-        font.fixedFamily === font.family
-          ? "Family"
-          : "Family (Identified by Typst)"
-      ),
+      code(font.fixedFamily === font.family ? "Family" : "Family (Identified by Typst)"),
       ": ",
-      code(
-        font.fixedFamily === font.family
-          ? font.family
-          : `${font.family} (${font.fixedFamily})`
-      )
+      code(font.fixedFamily === font.family ? font.family : `${font.family} (${font.fixedFamily})`),
     );
   };
 
   const ArgSlots = () => {
     const res: ChildDom[] = [];
-    let val = serverInfos.val["primary"];
+    const keys = Object.keys(serverInfos.val);
+    if (keys.length === 0) {
+      return res;
+    }
+
+    const firstKey = keys[0];
+
+    let val = serverInfos.val[firstKey];
+    if (!val) {
+      return res;
+    }
+
+    if (firstKey !== "primary") {
+      res.push(div(a({ href: "javascript:void(0)" }, code("id")), ": ", code(firstKey)));
+    }
+
     if (val.root) {
-      res.push(
-        div(
-          a({ href: "javascript:void(0)" }, code("root")),
-          ": ",
-          code(val.root)
-        )
-      );
+      res.push(div(a({ href: "javascript:void(0)" }, code("root")), ": ", code(val.root)));
     }
 
     for (let i = 0; i < val.fontPaths.length; i++) {
@@ -131,8 +117,8 @@ export const Summary = () => {
         div(
           a({ href: "javascript:void(0)" }, code(`font-path(${i})`)),
           ": ",
-          code(val.fontPaths[i])
-        )
+          code(val.fontPaths[i]),
+        ),
       );
     }
 
@@ -142,26 +128,21 @@ export const Summary = () => {
         codeList.push(
           span({ style: "color: #DEC76E" }, key),
           span({ style: "color: #7DCFFF" }, "="),
-          val.inputs[key]
+          val.inputs[key],
         );
       }
 
-      res.push(
-        div(
-          a({ href: "javascript:void(0)" }, code("sys.inputs")),
-          ": ",
-          code(...codeList)
-        )
-      );
+      res.push(div(a({ href: "javascript:void(0)" }, code("sys.inputs")), ": ", code(...codeList)));
 
       for (const [key, htmlContent] of Object.entries(val.stats)) {
         res.push(
-          div(
-            div({ href: "javascript:void(0)" }, code(key)),
-            div({ innerHTML: htmlContent })
-          )
+          div(div({ href: "javascript:void(0)" }, code(key)), div({ innerHTML: htmlContent })),
         );
       }
+    }
+
+    if (keys.length > 1) {
+      res.push(div(a({ href: "javascript:void(0)" }, code("allIds")), ": ", code(keys.toString())));
     }
 
     return res;
@@ -174,10 +155,8 @@ export const Summary = () => {
     },
     div(
       { class: `tinymist-card`, style: "flex: 1; width: 100%; padding: 10px" },
-      div(
-        van.derive(() => `This document is compiled with following arguments.`)
-      ),
-      div({ style: "margin: 1.2em; margin-left: 0.5em" }, ...ArgSlots())
+      div(van.derive(() => `This document is compiled with following arguments.`)),
+      div({ style: "margin: 1.2em; margin-left: 0.5em" }, ...ArgSlots()),
     ),
     div(
       { class: `tinymist-card`, style: "flex: 1; width: 100%; padding: 10px" },
@@ -191,25 +170,20 @@ export const Summary = () => {
               startModal(
                 div(
                   {
-                    style:
-                      "height: calc(100% - 20px); box-sizing: border-box; padding-top: 4px",
+                    style: "height: calc(100% - 20px); box-sizing: border-box; padding-top: 4px",
                   },
                   fontsExportPanel({
                     fonts: docMetrics.val.fontInfo,
                     sources: docMetrics.val.spanInfo.sources,
-                  })
-                )
+                  }),
+                ),
               );
             },
           },
-          CopyIcon()
-        )
+          CopyIcon(),
+        ),
       ),
-      div(
-        van.derive(
-          () => `This document uses ${docMetrics.val.fontInfo.length} fonts.`
-        )
-      ),
+      div(van.derive(() => `This document uses ${docMetrics.val.fontInfo.length} fonts.`)),
       (_dom?: Element) =>
         div(
           ...docMetrics.val.fontInfo
@@ -229,8 +203,8 @@ export const Summary = () => {
               }
               return x.name.localeCompare(y.name);
             })
-            .map(FontSlot)
-        )
+            .map(FontSlot),
+        ),
     ),
     div(
       {
@@ -240,18 +214,14 @@ export const Summary = () => {
       div(`The Tinymist service.`),
       div(
         { style: "margin: 0.8em; margin-left: 0.5em" },
-        div(
-          `Its version is `,
-          a({ href: "javascript:void(0)" }, "0.11.6"),
-          `.`
-        ),
+        div(`Its version is `, a({ href: "javascript:void(0)" }, "0.11.6"), `.`),
         div(`It is compiled with optimization level `, "3", `.`),
         div(
           `It is connecting to the client `,
           code({ style: "font-style: italic" }, "VSCode 1.87.2"),
-          `.`
-        )
-      )
+          `.`,
+        ),
+      ),
     ),
     div(
       {
@@ -261,17 +231,9 @@ export const Summary = () => {
       div(`The Typst compiler.`),
       div(
         { style: "margin: 0.8em; margin-left: 0.5em" },
-        div(
-          `Its version is `,
-          a({ href: "javascript:void(0)" }, "0.11.0"),
-          `.`
-        ),
-        div(
-          `It identifies `,
-          a({ href: "javascript:void(0)" }, "374"),
-          ` font variants.`
-        )
-      )
+        div(`Its version is `, a({ href: "javascript:void(0)" }, "0.11.0"), `.`),
+        div(`It identifies `, a({ href: "javascript:void(0)" }, "374"), ` font variants.`),
+      ),
     ),
     div(
       {
@@ -286,20 +248,12 @@ export const Summary = () => {
           { style: "margin-left: 0.5em" },
           a({ href: "javascript:void(0)", style: "color: #DEC76E" }, "columns"),
           span({ style: "color: #7DCFFF" }, "="),
-          "120"
+          "120",
         ),
-        div(
-          `The version of typstyle is `,
-          a({ href: "javascript:void(0)" }, "0.11.7"),
-          `.`
-        ),
-        div(
-          `The version of typstfmt is `,
-          a({ href: "javascript:void(0)" }, "0.2.9"),
-          `.`
-        )
-      )
-    )
+        div(`The version of typstyle is `, a({ href: "javascript:void(0)" }, "0.11.7"), `.`),
+        div(`The version of typstfmt is `, a({ href: "javascript:void(0)" }, "0.2.9"), `.`),
+      ),
+    ),
   );
 };
 
@@ -326,10 +280,7 @@ interface csvFieldExtractor<H, T> {
   extractor: (input: T) => string | number;
 }
 
-type fontCSVFieldExtractor = csvFieldExtractor<
-  fontsCSVHeader,
-  fontInfoWithSource
->;
+type fontCSVFieldExtractor = csvFieldExtractor<fontsCSVHeader, fontInfoWithSource>;
 
 class fontsCSVGenerator {
   public static readonly fieldExtractors: fontCSVFieldExtractor[] = [
@@ -372,18 +323,13 @@ class fontsCSVGenerator {
     },
   ];
 
-  public generate(
-    fonts: fontInfoWithSource[],
-    config: fontsExportCSVConfigure
-  ): string {
+  public generate(fonts: fontInfoWithSource[], config: fontsExportCSVConfigure): string {
     const fields = fontsCSVGenerator.fieldExtractors.filter((field) =>
-      config.fields.includes(field.fieldName)
+      config.fields.includes(field.fieldName),
     );
     const headers = fields.map((field) => field.fieldName);
 
-    let rows = fonts.map((font) =>
-      fields.map((field) => field.extractor(font))
-    );
+    let rows = fonts.map((font) => fields.map((field) => field.extractor(font)));
 
     // If only field is file path, do a dedupp
     if (fields.length === 1 && fields[0].fieldName === "path") {
@@ -397,7 +343,7 @@ class fontsCSVGenerator {
           }
           return acc;
         },
-        [] as typeof rows
+        [] as typeof rows,
       );
     }
 
@@ -409,9 +355,7 @@ class fontsCSVGenerator {
   }
 }
 
-export type fontLocation = FontSource extends { kind: infer Kind }
-  ? Kind
-  : never;
+export type fontLocation = FontSource extends { kind: infer Kind } ? Kind : never;
 
 export interface fontsExportCSVConfigure {
   header: boolean;
@@ -437,8 +381,7 @@ interface fontsExportCommonConfigure {
   };
 }
 
-export type fontsExportConfigure = fontsExportCommonConfigure &
-  fontsExportFormatConfigure;
+export type fontsExportConfigure = fontsExportCommonConfigure & fontsExportFormatConfigure;
 
 // todo: deduplicate me. it also occurs in editors/vscode/src/editor-tools.ts
 export const fontsExportDefaultConfigure: fontsExportConfigure = {
@@ -459,20 +402,14 @@ export const fontsExportDefaultConfigure: fontsExportConfigure = {
 let savedConfigureData = `:[[preview:FontsExportConfigure]]:`;
 
 const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
-  const savedConfigure: fontsExportConfigure = savedConfigureData.startsWith(
-    ":"
-  )
+  const savedConfigure: fontsExportConfigure = savedConfigureData.startsWith(":")
     ? fontsExportDefaultConfigure
     : JSON.parse(base64Decode(savedConfigureData));
 
   const exportFormat = van.state<fontsExportFormat>(savedConfigure.format);
-  const locationFilter = van.state<fontLocation[]>(
-    savedConfigure.filters.location
-  );
+  const locationFilter = van.state<fontLocation[]>(savedConfigure.filters.location);
   const csvConfigure = van.state<fontsExportCSVConfigure>(savedConfigure.csv);
-  const jsonConfigure = van.state<fontsExportJSONConfigure>(
-    savedConfigure.json
-  );
+  const jsonConfigure = van.state<fontsExportJSONConfigure>(savedConfigure.json);
 
   // Save state when changed
   van.derive(() => {
@@ -495,9 +432,7 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
   });
 
   const filteredData = van.derive(() => {
-    return data.filter((font) =>
-      locationFilter.val.includes(font.source?.kind ?? ("" as any))
-    );
+    return data.filter((font) => locationFilter.val.includes(font.source?.kind ?? ("" as any)));
   });
 
   const exportText = van.derive<string>(() => {
@@ -519,27 +454,17 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
   const itemGap = 10;
   const groupGap = 20;
 
-  const labeledInput = (
-    title: string,
-    el: HTMLInputElement,
-    { labelStyle } = { labelStyle: "" }
-  ) =>
+  const labeledInput = (title: string, el: HTMLInputElement, { labelStyle } = { labelStyle: "" }) =>
     span(
       {
         style: `display: inline-flex; column-gap: ${labelInputGap}px; align-items: center`,
       },
       label({ for: el.id, style: labelStyle }, title),
-      el
+      el,
     );
 
-  const makeArrayCheckbox = (
-    id: string,
-    value: string,
-    state: State<string[]> | string[]
-  ) => {
-    const checked = Array.isArray(state)
-      ? state.includes(value)
-      : state.val.includes(value);
+  const makeArrayCheckbox = (id: string, value: string, state: State<string[]> | string[]) => {
+    const checked = Array.isArray(state) ? state.includes(value) : state.val.includes(value);
     return input({
       id,
       type: "checkbox",
@@ -578,18 +503,14 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
           },
           labeledInput(
             "FileSystem",
-            makeArrayCheckbox("filter-locations-fs", "fs", locationFilter)
+            makeArrayCheckbox("filter-locations-fs", "fs", locationFilter),
           ),
           labeledInput(
             "Memory",
-            makeArrayCheckbox(
-              "filter-locations-memory",
-              "memory",
-              locationFilter
-            )
-          )
-        )
-      )
+            makeArrayCheckbox("filter-locations-memory", "memory", locationFilter),
+          ),
+        ),
+      ),
     );
 
   const chooseExportFormatUI = () =>
@@ -614,7 +535,7 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
                 exportFormat.val = "csv";
               }
             },
-          })
+          }),
         ),
         labeledInput(
           "JSON",
@@ -629,9 +550,9 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
                 exportFormat.val = "json";
               }
             },
-          })
-        )
-      )
+          }),
+        ),
+      ),
     );
 
   const csvConfigureUI = () =>
@@ -660,7 +581,7 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
               style: "margin: 0px",
               checked: csvConfigure.val.header,
               onchange: (e) => (csvConfigure.rawVal.header = e.target.checked),
-            })
+            }),
           ),
           labeledInput(
             "Delimiter:",
@@ -671,9 +592,9 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
               value: csvConfigure.val.delimiter,
               oninput: (e) => (csvConfigure.rawVal.delimiter = e.target.value),
               onkeydown: (e) => e.stopPropagation(), // prevent modal window closed by space when input
-            })
-          )
-        )
+            }),
+          ),
+        ),
       ),
       div(
         { class: "flex-row", style: "align-items: center" },
@@ -688,15 +609,11 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
             .map((field) =>
               labeledInput(
                 field,
-                makeArrayCheckbox(
-                  `csv-field-${field}`,
-                  field,
-                  csvConfigure.rawVal.fields
-                )
-              )
-            )
-        )
-      )
+                makeArrayCheckbox(`csv-field-${field}`, field, csvConfigure.rawVal.fields),
+              ),
+            ),
+        ),
+      ),
     );
 
   const jsonConfigureUI = () =>
@@ -725,13 +642,12 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
               max: "8",
               step: "2",
               value: jsonConfigure.val.indent,
-              onchange: (e) =>
-                (jsonConfigure.rawVal.indent = parseInt(e.target.value, 10)),
+              onchange: (e) => (jsonConfigure.rawVal.indent = parseInt(e.target.value, 10)),
             }),
-            { labelStyle: "margin-right: 0.5em" }
-          )
-        )
-      )
+            { labelStyle: "margin-right: 0.5em" },
+          ),
+        ),
+      ),
     );
 
   const exportFormatConfigureUI = () => {
@@ -765,7 +681,7 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
         readOnly: true,
         onkeydown: (e) => e.stopPropagation(),
       },
-      exportText
+      exportText,
     ),
     div(
       { style: `display: flex; align-items: center; column-gap:${itemGap}px` },
@@ -785,7 +701,7 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
             });
           },
         },
-        "Export"
+        "Export",
       ),
       button(
         {
@@ -793,9 +709,9 @@ const fontsExportPanel = ({ fonts, sources }: fontsExportPanelProps) => {
           style: "flex: 1",
           onclick: () => navigator.clipboard.writeText(exportText.val),
         },
-        "Copy"
-      )
-    )
+        "Copy",
+      ),
+    ),
   );
 };
 
