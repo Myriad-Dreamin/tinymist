be401f0dc15f96b7923eed53bae595a3406fffb7
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-14 11:39:54 +0800

    dev: remove dirty log

--- editors/vscode/src/l10n.ts
+++ editors/vscode/src/l10n.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
 
 export function l10nStr(message: string, args?: Record<string, string | number | boolean>): string {
-  console.log("vscode.l10n.uri", vscode.l10n.uri);
   return vscode.l10n.t(message, args || {});
 }
