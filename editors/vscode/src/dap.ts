5f672bc4626e3ce3a6684bfb03fe9762bd51d690
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-15 21:36:58 +0800

    feat: clean up client-side code

--- editors/vscode/src/dap.ts
+++ editors/vscode/src/dap.ts
@@ -1,143 +1,60 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-/*
- * activateMockDebug.ts containes the shared extension code that can be executed both in node.js and the browser.
- */
-
 "use strict";
 
 import * as vscode from "vscode";
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from "vscode";
-import { TypstDebugSession } from "./dap/mockDebug";
-import { FileAccessor } from "./dap/mockRuntime";
 
 export const TYPST_DEBUGGER_TYPE = "myriaddreamin.typst-debugger";
 
-// import { promises as fs } from "fs";
-// import * as Net from "net";
-// import { FileAccessor } from "./mockRuntime";
+export function debugActivate(context: vscode.ExtensionContext) {
+  const factory: vscode.DebugAdapterDescriptorFactory = new DebugAdapterExecutableFactory();
 
-// /*
-//  * debugAdapter.js is the entrypoint of the debug adapter when it runs as a separate process.
-//  */
-
-// /*
-//  * Since here we run the debug adapter as a separate ("external") process, it has no access to VS Code API.
-//  * So we can only use node.js API for accessing files.
-//  */
-// const fsAccessor: FileAccessor = {
-//   isWindows: process.platform === "win32",
-//   readFile(path: string): Promise<Uint8Array> {
-//     return fs.readFile(path);
-//   },
-//   writeFile(path: string, contents: Uint8Array): Promise<void> {
-//     return fs.writeFile(path, contents);
-//   },
-// };
-
-// /*
-//  * When the debug adapter is run as an external process,
-//  * normally the helper function DebugSession.run(...) takes care of everything:
-//  *
-//  * 	MockDebugSession.run(MockDebugSession);
-//  *
-//  * but here the helper is not flexible enough to deal with a debug session constructors with a parameter.
-//  * So for now we copied and modified the helper:
-//  */
-
-// // first parse command line arguments to see whether the debug adapter should run as a server
-// let port = 0;
-// const args = process.argv.slice(2);
-// args.forEach(function (val, index, array) {
-//   const portMatch = /^--server=(\d{4,5})$/.exec(val);
-//   if (portMatch) {
-//     port = parseInt(portMatch[1], 10);
-//   }
-// });
-
-// if (port > 0) {
-//   // start a server that creates a new session for every connection request
-//   console.error(`waiting for debug protocol on port ${port}`);
-//   Net.createServer((socket) => {
-//     console.error(">> accepted connection from client");
-//     socket.on("end", () => {
-//       console.error(">> client connection closed\n");
-//     });
-//     const session = new TypstDebugSession(fsAccessor);
-//     session.setRunAsServer(true);
-//     session.start(socket, socket);
-//   }).listen(port);
-// } else {
-//   // start a single session that communicates via stdin/stdout
-//   const session = new TypstDebugSession(fsAccessor);
-//   process.on("SIGTERM", () => {
-//     session.shutdown();
-//   });
-//   session.start(process.stdin, process.stdout);
-// }
-
-export function activateMockDebug(
-  context: vscode.ExtensionContext,
-  factory?: vscode.DebugAdapterDescriptorFactory,
-) {
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "extension.mock-debug.runEditorContents",
-      (resource: vscode.Uri) => {
-        let targetResource = resource;
-        if (!targetResource && vscode.window.activeTextEditor) {
-          targetResource = vscode.window.activeTextEditor.document.uri;
-        }
-        if (targetResource) {
-          vscode.debug.startDebugging(
-            undefined,
-            {
-              type: TYPST_DEBUGGER_TYPE,
-              name: "Run File",
-              request: "launch",
-              program: targetResource.fsPath,
-            },
-            { noDebug: true },
-          );
-        }
-      },
-    ),
-    vscode.commands.registerCommand(
-      "extension.mock-debug.debugEditorContents",
-      (resource: vscode.Uri) => {
-        let targetResource = resource;
-        if (!targetResource && vscode.window.activeTextEditor) {
-          targetResource = vscode.window.activeTextEditor.document.uri;
-        }
-        if (targetResource) {
-          vscode.debug.startDebugging(undefined, {
+    vscode.commands.registerCommand("tinymist.runEditorContents", (resource: vscode.Uri) => {
+      let targetResource = resource;
+      if (!targetResource && vscode.window.activeTextEditor) {
+        targetResource = vscode.window.activeTextEditor.document.uri;
+      }
+      if (targetResource) {
+        vscode.debug.startDebugging(
+          undefined,
+          {
             type: TYPST_DEBUGGER_TYPE,
-            name: "Debug File",
+            name: "Run File",
             request: "launch",
             program: targetResource.fsPath,
-            stopOnEntry: true,
-          });
-        }
-      },
-    ),
-    vscode.commands.registerCommand("extension.mock-debug.toggleFormatting", (variable) => {
-      const ds = vscode.debug.activeDebugSession;
-      if (ds) {
-        ds.customRequest("toggleFormatting");
+          },
+          { noDebug: true },
+        );
       }
     }),
+    vscode.commands.registerCommand("tinymist.debugEditorContents", (resource: vscode.Uri) => {
+      let targetResource = resource;
+      if (!targetResource && vscode.window.activeTextEditor) {
+        targetResource = vscode.window.activeTextEditor.document.uri;
+      }
+      if (targetResource) {
+        vscode.debug.startDebugging(undefined, {
+          type: TYPST_DEBUGGER_TYPE,
+          name: "Debug File",
+          request: "launch",
+          program: targetResource.fsPath,
+          stopOnEntry: true,
+        });
+      }
+    }),
+    // vscode.commands.registerCommand("extension.mock-debug.toggleFormatting", (variable) => {
+    //   const ds = vscode.debug.activeDebugSession;
+    //   if (ds) {
+    //     ds.customRequest("toggleFormatting");
+    //   }
+    // }),
   );
 
   context.subscriptions.push(
     vscode.commands.registerCommand("myriaddreamin.typst-debugger.getProgramName", (config) => {
       return vscode.window.showInputBox({
         placeHolder: "Please enter the name of a typst file in the workspace folder",
-        value: "test.typ",
+        value: "main.typ",
       });
     }),
   );
@@ -145,7 +62,7 @@ export function activateMockDebug(
   // register a configuration provider for 'mock' debug type
   const provider = new MockConfigurationProvider();
   context.subscriptions.push(
-    vscode.debug.registerDebugConfigurationProvider("myriaddeamin.typst-debugger", provider),
+    vscode.debug.registerDebugConfigurationProvider(TYPST_DEBUGGER_TYPE, provider),
   );
 
   // register a dynamic configuration provider for 'mock' debug type
@@ -153,24 +70,10 @@ export function activateMockDebug(
     vscode.debug.registerDebugConfigurationProvider(
       TYPST_DEBUGGER_TYPE,
       {
-        provideDebugConfigurations(
-          folder: WorkspaceFolder | undefined,
-        ): ProviderResult<DebugConfiguration[]> {
+        provideDebugConfigurations(): ProviderResult<DebugConfiguration[]> {
           return [
             {
               name: "Dynamic Launch",
-              request: "launch",
-              type: TYPST_DEBUGGER_TYPE,
-              program: "${file}",
-            },
-            {
-              name: "Another Dynamic Launch",
-              request: "launch",
-              type: TYPST_DEBUGGER_TYPE,
-              program: "${file}",
-            },
-            {
-              name: "Mock Launch",
               request: "launch",
               type: TYPST_DEBUGGER_TYPE,
               program: "${file}",
@@ -182,9 +85,6 @@ export function activateMockDebug(
     ),
   );
 
-  if (!factory) {
-    factory = new InlineDebugAdapterFactory();
-  }
   context.subscriptions.push(
     vscode.debug.registerDebugAdapterDescriptorFactory(TYPST_DEBUGGER_TYPE, factory),
   );
@@ -222,40 +122,40 @@ export function activateMockDebug(
   );
 
   // override VS Code's default implementation of the "inline values" feature"
-  context.subscriptions.push(
-    vscode.languages.registerInlineValuesProvider("typst", {
-      provideInlineValues(
-        document: vscode.TextDocument,
-        viewport: vscode.Range,
-        context: vscode.InlineValueContext,
-      ): vscode.ProviderResult<vscode.InlineValue[]> {
-        const allValues: vscode.InlineValue[] = [];
+  // context.subscriptions.push(
+  //   vscode.languages.registerInlineValuesProvider("typst", {
+  //     provideInlineValues(
+  //       document: vscode.TextDocument,
+  //       viewport: vscode.Range,
+  //       context: vscode.InlineValueContext,
+  //     ): vscode.ProviderResult<vscode.InlineValue[]> {
+  //       const allValues: vscode.InlineValue[] = [];
 
-        for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
-          const line = document.lineAt(l);
-          var regExp = /\$([a-z][a-z0-9]*)/gi; // variables are words starting with '$'
-          do {
-            var m = regExp.exec(line.text);
-            if (m) {
-              const varName = m[1];
-              const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
+  //       for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
+  //         const line = document.lineAt(l);
+  //         var regExp = /\$([a-z][a-z0-9]*)/gi; // variables are words starting with '$'
+  //         do {
+  //           var m = regExp.exec(line.text);
+  //           if (m) {
+  //             const varName = m[1];
+  //             const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
 
-              // some literal text
-              //allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
+  //             // some literal text
+  //             //allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
 
-              // value found via variable lookup
-              allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
+  //             // value found via variable lookup
+  //             allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
 
-              // value determined via expression evaluation
-              //allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
-            }
-          } while (m);
-        }
+  //             // value determined via expression evaluation
+  //             //allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
+  //           }
+  //         } while (m);
+  //       }
 
-        return allValues;
-      },
-    }),
-  );
+  //       return allValues;
+  //     },
+  //   }),
+  // );
 }
 
 class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
@@ -290,37 +190,50 @@ class MockConfigurationProvider implements vscode.DebugConfigurationProvider {
   }
 }
 
-export const workspaceFileAccessor: FileAccessor = {
-  isWindows: typeof process !== "undefined" && process.platform === "win32",
-  async readFile(path: string): Promise<Uint8Array> {
-    let uri: vscode.Uri;
-    try {
-      uri = pathToUri(path);
-    } catch (e) {
-      return new TextEncoder().encode(`cannot read '${path}'`);
+class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFactory {
+  // The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
+  // Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
+  createDebugAdapterDescriptor(
+    session: vscode.DebugSession,
+    executable: vscode.DebugAdapterExecutable | undefined,
+  ): ProviderResult<vscode.DebugAdapterDescriptor> {
+    const isProdMode = true; // context.extensionMode === ExtensionMode.Production;
+
+    // param "executable" contains the executable optionally specified in the package.json (if any)
+    console.log("executable from configuration", executable);
+
+    /// Set the `RUST_BACKTRACE` environment variable to `full` to print full backtrace on error. This is useless in
+    /// production mode because we don't put the debug information in the binary.
+    ///
+    /// Note: Developers can still download the debug information from the GitHub Releases and enable the backtrace
+    /// manually by themselves.
+    const RUST_BACKTRACE = isProdMode ? "1" : "full";
+
+    // use the executable specified in the package.json if it exists or determine it based on some other information (e.g. the session)
+    if (!executable) {
+      const command = "absolute path to my DA executable";
+      const args = ["dap"];
+      const options = {
+        cwd: "working directory for executable",
+        env: { RUST_BACKTRACE },
+      };
+      executable = new vscode.DebugAdapterExecutable(command, args, options);
     }
 
-    return await vscode.workspace.fs.readFile(uri);
-  },
-  async writeFile(path: string, contents: Uint8Array) {
-    await vscode.workspace.fs.writeFile(pathToUri(path), contents);
-  },
-};
+    const command = executable.command;
+    const rootCwd = executable.options?.cwd || session.workspaceFolder?.uri.fsPath;
+    const args = ["dap"];
 
-function pathToUri(path: string) {
-  try {
-    return vscode.Uri.file(path);
-  } catch (e) {
-    return vscode.Uri.parse(path);
-  }
-}
+    const options = executable.options || {};
+    options.cwd = rootCwd;
+    options.env = Object.assign({}, process.env, {
+      ...(options.env || {}),
+      RUST_BACKTRACE,
+    }) as Record<string, string>;
 
-class InlineDebugAdapterFactory implements vscode.DebugAdapterDescriptorFactory {
-  createDebugAdapterDescriptor(
-    _session: vscode.DebugSession,
-  ): ProviderResult<vscode.DebugAdapterDescriptor> {
-    return new vscode.DebugAdapterInlineImplementation(
-      new TypstDebugSession(workspaceFileAccessor),
-    );
+    executable = new vscode.DebugAdapterExecutable(command, args, options);
+
+    // make VS Code launch the DA executable
+    return executable;
   }
 }
