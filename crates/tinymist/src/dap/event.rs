c598694e3b4872ab0dc4fc7284a7fe3b3303aa27
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-17 11:51:35 +0800

    dev: remove comments

--- crates/tinymist/src/dap/event.rs
+++ crates/tinymist/src/dap/event.rs
@@ -1,17 +1,3 @@
-//  import {
-//     Thread,
-//     StackFrame,
-//     Scope,
-//     Source,
-//     Handles,
-//     Breakpoint,
-//     MemoryEvent,
-//   } from "@vscode/debugadapter";
-//   import { DebugProtocol } from "@vscode/debugprotocol";
-//   import { basename } from "path";
-//   import { Subject } from "await-notify";
-//   import * as base64 from "base64-js";
-
 use super::*;
 
 impl ServerState {}
