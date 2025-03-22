8fa4ef386e067df1f298b753af7717a0a387c526
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-22 18:38:58 +0800

    feat: improve repr

--- crates/tinymist-query/src/cf/repr.rs
+++ crates/tinymist-query/src/cf/repr.rs
@@ -128,15 +128,14 @@ impl CfPrinter<'_> {
             CfInstr::Loop(cf_loop) => {
                 write!(f, ": {}", cf_loop.ty.repr_any())?;
                 write!(f, " = loop cond %{}", cf_loop.cond)?;
-                write!(f, " body ^{}", cf_loop.body)?;
+                write!(f, " body ^bb{}", cf_loop.body)?;
                 if let Some(cont) = cf_loop.cont {
-                    write!(f, " cont ^{cont}")?;
+                    write!(f, " cont ^bb{cont}")?;
                 }
             }
             CfInstr::Block(cf_block) => {
                 write!(f, ": {}", cf_block.ty.repr_any())?;
-                write!(f, " = block")?;
-                write!(f, " ^{}", cf_block.body)?;
+                write!(f, " = ^bb{}", cf_block.body)?;
             }
             CfInstr::Element(cf_element) => {
                 // pub elem: Element,
@@ -144,36 +143,36 @@ impl CfPrinter<'_> {
 
                 write!(
                     f,
-                    " = elem({}, ^{})",
+                    " = elem({}, ^bb{})",
                     cf_element.elem.name(),
                     cf_element.body
                 )?;
             }
             CfInstr::Contextual(index_vec_idx) => {
-                write!(f, " = context ^{index_vec_idx}")?;
+                write!(f, " = context ^bb{index_vec_idx}")?;
             }
             CfInstr::Show(cf_show) => {
-                write!(f, " = show ^{}", cf_show.cont)?;
+                write!(f, " = show ^bb{}", cf_show.cont)?;
                 if let Some(selector) = cf_show.selector {
                     write!(f, " select %{selector}")?;
                 }
                 write!(f, " using %{}", cf_show.edit)?;
             }
             CfInstr::Set(cf_set) => {
-                write!(f, " = set ^{}", cf_set.cont)?;
+                write!(f, " = set ^bb{}", cf_set.cont)?;
                 write!(f, " %{}", cf_set.target)?;
                 write!(f, " with %{}", cf_set.args)?;
             }
             CfInstr::Break(cont) => {
                 if let Some(cont) = cont {
-                    write!(f, " = break ^{cont}")?;
+                    write!(f, " = break ^bb{cont}")?;
                 } else {
                     write!(f, " = break undef")?;
                 }
             }
             CfInstr::Continue(cont) => {
                 if let Some(cont) = cont {
-                    write!(f, " = continue ^{cont}")?;
+                    write!(f, " = continue ^bb{cont}")?;
                 } else {
                     write!(f, " = continue undef")?;
                 }
@@ -186,7 +185,7 @@ impl CfPrinter<'_> {
                 }
             }
             CfInstr::Branch(index_vec_idx) => {
-                write!(f, " = branch ^{index_vec_idx}")?;
+                write!(f, " = branch ^bb{index_vec_idx}")?;
             }
             CfInstr::Meta(expr) => {
                 write!(f, " = meta {}", expr.repr())?;
