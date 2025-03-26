43db349eab0ae8f2f3879429469012014e989bf8
Myriad-Dreamin <camiyoru@gmail.com>
2025-03-27 00:12:00 +0800

    fix: fuzzy search

--- crates/tinymist-query/src/jump.rs
+++ crates/tinymist-query/src/jump.rs
@@ -103,22 +103,36 @@ fn jump_from_cursor_(
     source: &Source,
     cursor: usize,
 ) -> Option<Vec<Position>> {
+    let node = LinkedNode::new(source.root())
+        .leaf_at_compat(cursor)
+        .filter(|node| !matches!(node.kind(), SyntaxKind::Text | SyntaxKind::MathText))?;
+
+    let span = node.span();
     match document {
         TypstDocument::Paged(paged_doc) => {
-            let node = LinkedNode::new(source.root())
-                .leaf_at_compat(cursor)
-                .filter(|node| !matches!(node.kind(), SyntaxKind::Text | SyntaxKind::MathText))?;
-
-            let span = node.span();
             let mut positions = vec![];
+            let mut min_page = 0;
+            let mut min_point = Point::default();
+            let mut min_dis = u64::MAX;
             for (idx, page) in paged_doc.pages.iter().enumerate() {
-                let mut min_dis = u64::MAX;
-                let mut point = Point::default();
-                if let Some(point) = find_in_frame(&page.frame, span, &mut min_dis, &mut point) {
+                let mut t_dis = min_dis;
+                if let Some(point) = find_in_frame(&page.frame, span, &mut t_dis, &mut min_point) {
                     if let Some(page) = NonZeroUsize::new(idx + 1) {
                         positions.push(Position { page, point });
                     }
                 }
+
+                if t_dis != min_dis {
+                    min_page = idx;
+                    min_dis = t_dis;
+                }
+            }
+
+            if positions.is_empty() && min_dis != u64::MAX {
+                positions.push(Position {
+                    page: NonZeroUsize::new(min_page + 1)?,
+                    point: min_point,
+                });
             }
 
             Some(positions)
@@ -200,7 +214,7 @@ mod tests {
                         .map(|pos| {
                             let page = pos.page.get();
                             let point = pos.point;
-                            format!("{page},{point:?}")
+                            format!("{page},{:.3}pt,{:.3}pt", point.x.to_pt(), point.y.to_pt())
                         })
                         .join(";")
                 })
