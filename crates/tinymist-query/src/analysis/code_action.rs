e2d7786d1baa06ed29148f413358f367b09d209f
Myriad-Dreamin <camiyoru@gmail.com>
2025-04-22 03:38:55 +0800

    feat: implement matchers

--- crates/tinymist-query/src/analysis/code_action.rs
+++ crates/tinymist-query/src/analysis/code_action.rs
@@ -1,10 +1,13 @@
 //! Provides code actions for the document.
 
 use regex::Regex;
+use tinymist_analysis::syntax::{adjust_expr, node_ancestors, SyntaxClass};
 
-use crate::analysis::{get_link_exprs_in, LinkTarget};
+use crate::analysis::LinkTarget;
 use crate::prelude::*;
 use crate::syntax::{interpret_mode_at, InterpretMode};
+
+use super::get_link_exprs_in;
 
 /// Analyzes the document and provides code actions.
 pub struct CodeActionWorker<'a> {
@@ -56,9 +59,7 @@ impl<'a> CodeActionWorker<'a> {
 
         let mut heading_resolved = false;
         let mut equation_resolved = false;
-
-        // Only if the node is a string, try to provide path conversion actions
-        let start_as_str = node.kind() == SyntaxKind::Str;
+        let mut path_resolved = false;
 
         self.wrap_actions(node, range);
 
@@ -74,10 +75,9 @@ impl<'a> CodeActionWorker<'a> {
                     equation_resolved = true;
                     self.equation_actions(node);
                 }
-                // TODO: resolve path of SyntaxKind::ModuleImport | SyntaxKind::ModuleInclude
-                SyntaxKind::FuncCall if start_as_str => {
-                    self.path_actions(cursor, node);
-                    return Some(());
+                SyntaxKind::Str if !path_resolved => {
+                    path_resolved = true;
+                    self.path_actions(node, cursor);
                 }
                 _ => {}
             }
@@ -86,111 +86,146 @@ impl<'a> CodeActionWorker<'a> {
         }
     }
 
-    fn path_actions(&mut self, cursor: usize, node: &LinkedNode<'_>) -> Option<()> {
-        if let Some(link_info) = get_link_exprs_in(node) {
-            // Actually there should be only one link left
-            for link in link_info
-                .objects
-                .into_iter()
-                .filter(|link| link.range.contains(&cursor))
-            {
+    fn path_actions(&mut self, node: &LinkedNode, cursor: usize) -> Option<()> {
+        // We can only process on the cases that import path is a string.
+        if let Some(SyntaxClass::ImportPath(path_node)) = classify_syntax(node.clone(), cursor) {
+            let str_node = adjust_expr(path_node)?;
+            let str_ast = str_node.cast::<ast::Str>()?;
+            return self.path_rewrite(self.source.id(), &str_ast.get(), &str_node);
+        }
+
+        let link_parent = node_ancestors(node)
+            .find(|node| {
+                use SyntaxKind::*;
+                matches!(node.kind(), FuncCall | ModuleInclude | ModuleImport)
+            })
+            .unwrap_or(node);
+
+        // Actually there should be only one link left
+        if let Some(link_info) = get_link_exprs_in(link_parent) {
+            let objects = link_info.objects.into_iter();
+            let object_under_node = objects.filter(|link| link.range.contains(&cursor));
+
+            let mut resolved = false;
+            for link in object_under_node {
                 if let LinkTarget::Path(id, path) = link.target {
-                    let path = Path::new(path.as_str());
-                    if path.is_absolute() {
-                        // Convert absolute path to relative path
-
-                        let canonicalize = |path: &Path| {
-                            let mut path_buf = PathBuf::new();
-                            for component in path.components() {
-                                match component {
-                                    std::path::Component::ParentDir => {
-                                        path_buf.pop();
-                                    }
-                                    std::path::Component::CurDir => {}
-                                    component => {
-                                        path_buf.push(component);
-                                    }
-                                }
-                            }
-                            path_buf
-                        };
-
-                        let path = canonicalize(path);
-                        let mut path_iter = path.components();
-                        path_iter.next(); // skip the first `RootDir`
-                        let mut last_path_iter = path_iter.clone();
-                        let cur_path = id.vpath().as_rooted_path().parent().unwrap();
-                        let mut cur_path_iter = cur_path.components();
-                        cur_path_iter.next(); // skip the first `RootDir`
-                        let mut last_cur_path_iter = cur_path_iter.clone();
-
-                        // current `/a`, path `/b/c`, convert to `../b/c`
-                        let mut new_path = PathBuf::new();
-                        while let (
-                            Some(std::path::Component::Normal(name1)),
-                            Some(std::path::Component::Normal(name2)),
-                        ) = (cur_path_iter.next(), path_iter.next())
-                        {
-                            if name1 != name2 {
-                                break;
-                            }
-                            last_path_iter = path_iter.clone();
-                            last_cur_path_iter = cur_path_iter.clone();
-                        }
-
-                        for _ in last_cur_path_iter {
-                            new_path.push("..");
-                        }
-
-                        for component in last_path_iter {
-                            if let std::path::Component::Normal(name) = component {
-                                new_path.push(name);
-                            }
-                        }
-                        let new_path = new_path.to_string_lossy().to_string();
-                        let edit = self.local_edit(TextEdit {
-                            range: self.ctx.to_lsp_range(link.range, &self.source),
-                            new_text: new_path,
-                        })?;
-                        let action = CodeActionOrCommand::CodeAction(CodeAction {
-                            title: "Convert to relative path".to_string(),
-                            kind: Some(CodeActionKind::REFACTOR_REWRITE),
-                            edit: Some(edit),
-                            ..CodeAction::default()
-                        });
-                        self.actions.push(action);
-                    } else {
-                        // Convert relative path to absolute path
-                        let mut new_path =
-                            id.vpath().as_rooted_path().parent().unwrap().to_path_buf();
-                        for i in path.components() {
-                            match i {
-                                std::path::Component::ParentDir => {
-                                    new_path.pop().then_some(())?;
-                                }
-                                std::path::Component::Normal(name) => {
-                                    new_path.push(name);
-                                }
-                                _ => {}
-                            }
-                        }
-                        let new_path = new_path.to_string_lossy().to_string();
-                        let edit = self.local_edit(TextEdit {
-                            range: self.ctx.to_lsp_range(link.range, &self.source),
-                            new_text: new_path,
-                        })?;
-                        let action = CodeActionOrCommand::CodeAction(CodeAction {
-                            title: "Convert to absolute path".to_string(),
-                            kind: Some(CodeActionKind::REFACTOR_REWRITE),
-                            edit: Some(edit),
-                            ..CodeAction::default()
-                        });
-                        self.actions.push(action);
-                    }
+                    // todo: is there a link that is not a string?
+                    resolved = self.path_rewrite(id, &path, node).is_some() || resolved;
                 }
             }
+
+            return Some(());
         }
+
+        None
+    }
+
+    fn path_rewrite(&mut self, id: TypstFileId, path: &str, node: &LinkedNode) -> Option<()> {
+        if !matches!(node.kind(), SyntaxKind::Str) {
+            log::warn!("bad path node kind on code action: {:?}", node.kind());
+            return None;
+        }
+
+        let path = Path::new(path);
+
+        if path.is_absolute() {
+            // Convert absolute path to relative path
+            let canonicalize = |path: &Path| {
+                let mut path_buf = PathBuf::new();
+                for component in path.components() {
+                    match component {
+                        std::path::Component::ParentDir => {
+                            path_buf.pop();
+                        }
+                        std::path::Component::CurDir => {}
+                        component => {
+                            path_buf.push(component);
+                        }
+                    }
+                }
+                path_buf
+            };
+
+            let path = canonicalize(path);
+            let mut path_iter = path.components();
+            path_iter.next(); // skip the first `RootDir`
+            let mut last_path_iter = path_iter.clone();
+            let cur_path = id.vpath().as_rooted_path().parent().unwrap();
+            let mut cur_path_iter = cur_path.components();
+            cur_path_iter.next(); // skip the first `RootDir`
+            let mut last_cur_path_iter = cur_path_iter.clone();
+
+            // current `/a`, path `/b/c`, convert to `../b/c`
+            let mut new_path = PathBuf::new();
+            while let (
+                Some(std::path::Component::Normal(name1)),
+                Some(std::path::Component::Normal(name2)),
+            ) = (cur_path_iter.next(), path_iter.next())
+            {
+                if name1 != name2 {
+                    break;
+                }
+                last_path_iter = path_iter.clone();
+                last_cur_path_iter = cur_path_iter.clone();
+            }
+
+            for _ in last_cur_path_iter {
+                new_path.push("..");
+            }
+
+            for component in last_path_iter {
+                if let std::path::Component::Normal(name) = component {
+                    new_path.push(name);
+                }
+            }
+            let new_path = new_path.to_string_lossy().to_string();
+            let edit = self.edit_str(node, new_path)?;
+            let action = CodeActionOrCommand::CodeAction(CodeAction {
+                title: "Convert to relative path".to_string(),
+                kind: Some(CodeActionKind::REFACTOR_REWRITE),
+                edit: Some(edit),
+                ..CodeAction::default()
+            });
+            self.actions.push(action);
+        } else {
+            // Convert relative path to absolute path
+            let mut new_path = id.vpath().as_rooted_path().parent().unwrap().to_path_buf();
+            for i in path.components() {
+                match i {
+                    std::path::Component::ParentDir => {
+                        new_path.pop().then_some(())?;
+                    }
+                    std::path::Component::Normal(name) => {
+                        new_path.push(name);
+                    }
+                    _ => {}
+                }
+            }
+            let new_path = new_path.to_string_lossy().to_string();
+            let edit = self.edit_str(node, new_path)?;
+            let action = CodeActionOrCommand::CodeAction(CodeAction {
+                title: "Convert to absolute path".to_string(),
+                kind: Some(CodeActionKind::REFACTOR_REWRITE),
+                edit: Some(edit),
+                ..CodeAction::default()
+            });
+            self.actions.push(action);
+        }
+
         Some(())
+    }
+
+    fn edit_str(&mut self, node: &LinkedNode, new_content: String) -> Option<WorkspaceEdit> {
+        if !matches!(node.kind(), SyntaxKind::Str) {
+            log::warn!("edit_str only works on string AST nodes: {:?}", node.kind());
+            return None;
+        }
+
+        self.local_edit(TextEdit {
+            range: self.ctx.to_lsp_range(node.range(), &self.source),
+            // todo: this is merely ocasionally correct, abusing string escape (`fmt::Debug`)
+            new_text: format!("{new_content:?}"),
+        })
     }
 
     fn wrap_actions(&mut self, node: &LinkedNode, range: Range<usize>) -> Option<()> {
