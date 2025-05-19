5ecf874e1cd10fe576d839ab344b796b581cb7e3
Hong Jiarong <me@jrhim.com>
2025-05-19 23:06:47 +0800

    fix: nodes nested in HTML element should render as HTML

--- crates/typlite/src/common.rs
+++ crates/typlite/src/common.rs
@@ -1,7 +1,12 @@
 //! Common types and interfaces for the conversion system
 
-use cmark_writer::ast::{CustomNodeWriter, Node};
+use cmark_writer::ast::Node;
 use cmark_writer::custom_node;
+use cmark_writer::CommonMarkWriter;
+use cmark_writer::HtmlAttribute;
+use cmark_writer::HtmlElement;
+use cmark_writer::HtmlWriteResult;
+use cmark_writer::HtmlWriter;
 use cmark_writer::WriteResult;
 use ecow::EcoString;
 use std::path::PathBuf;
@@ -27,7 +32,7 @@ pub enum Format {
 
 /// Figure node implementation for all formats
 #[derive(Debug, PartialEq, Clone)]
-#[custom_node]
+#[custom_node(block = true, html_impl = true)]
 pub struct FigureNode {
     /// The main content of the figure, can be any block node
     pub body: Box<Node>,
@@ -36,8 +41,8 @@ pub struct FigureNode {
 }
 
 impl FigureNode {
-    fn write_custom(&self, writer: &mut dyn CustomNodeWriter) -> WriteResult<()> {
-        let mut temp_writer = cmark_writer::writer::CommonMarkWriter::new();
+    fn write_custom(&self, writer: &mut CommonMarkWriter) -> WriteResult<()> {
+        let mut temp_writer = CommonMarkWriter::new();
         temp_writer.write(&self.body)?;
         let content = temp_writer.into_string();
         writer.write_str(&content)?;
@@ -46,14 +51,25 @@ impl FigureNode {
         Ok(())
     }
 
-    fn is_block_custom(&self) -> bool {
-        true
+    fn write_html_custom(&self, writer: &mut HtmlWriter) -> HtmlWriteResult<()> {
+        let body = self.body.clone();
+        let node = Node::HtmlElement(HtmlElement {
+            tag: "figure".to_string(),
+            attributes: vec![HtmlAttribute {
+                name: "class".to_string(),
+                value: "figure".to_string(),
+            }],
+            children: vec![*body],
+            self_closing: false,
+        });
+        writer.write_node(&node)?;
+        Ok(())
     }
 }
 
 /// External Frame node for handling frames stored as external files
 #[derive(Debug, PartialEq, Clone)]
-#[custom_node]
+#[custom_node(block = true, html_impl = true)]
 pub struct ExternalFrameNode {
     /// The path to the external file containing the frame
     pub file_path: PathBuf,
@@ -64,7 +80,7 @@ pub struct ExternalFrameNode {
 }
 
 impl ExternalFrameNode {
-    fn write_custom(&self, writer: &mut dyn CustomNodeWriter) -> WriteResult<()> {
+    fn write_custom(&self, writer: &mut CommonMarkWriter) -> WriteResult<()> {
         // The actual handling is implemented in format-specific writers
         writer.write_str(&format!(
             "![{}]({})",
@@ -74,22 +90,38 @@ impl ExternalFrameNode {
         Ok(())
     }
 
-    fn is_block_custom(&self) -> bool {
-        true
+    fn write_html_custom(&self, writer: &mut HtmlWriter) -> HtmlWriteResult<()> {
+        let node = Node::HtmlElement(HtmlElement {
+            tag: "img".to_string(),
+            attributes: vec![
+                HtmlAttribute {
+                    name: "src".to_string(),
+                    value: self.file_path.display().to_string(),
+                },
+                HtmlAttribute {
+                    name: "alt".to_string(),
+                    value: self.alt_text.clone(),
+                },
+            ],
+            children: vec![],
+            self_closing: true,
+        });
+        writer.write_node(&node)?;
+        Ok(())
     }
 }
 
 /// Highlight node for highlighted text
 #[derive(Debug, PartialEq, Clone)]
-#[custom_node]
+#[custom_node(block = false, html_impl = true)]
 pub struct HighlightNode {
     /// The content to be highlighted
     pub content: Vec<Node>,
 }
 
 impl HighlightNode {
-    fn write_custom(&self, writer: &mut dyn CustomNodeWriter) -> WriteResult<()> {
-        let mut temp_writer = cmark_writer::writer::CommonMarkWriter::new();
+    fn write_custom(&self, writer: &mut CommonMarkWriter) -> WriteResult<()> {
+        let mut temp_writer = CommonMarkWriter::new();
         for node in &self.content {
             temp_writer.write(node)?;
         }
@@ -98,14 +130,19 @@ impl HighlightNode {
         Ok(())
     }
 
-    fn is_block_custom(&self) -> bool {
-        false
+    fn write_html_custom(&self, writer: &mut HtmlWriter) -> HtmlWriteResult<()> {
+        writer.start_tag("mark")?;
+        for node in &self.content {
+            writer.write_node(node)?;
+        }
+        writer.end_tag("mark")?;
+        Ok(())
     }
 }
 
 /// Node for centered content
 #[derive(Debug, PartialEq, Clone)]
-#[custom_node]
+#[custom_node(block = true, html_impl = true)]
 pub struct CenterNode {
     /// The content to be centered
     pub node: Node,
@@ -126,8 +163,8 @@ impl CenterNode {
         }
     }
 
-    fn write_custom(&self, writer: &mut dyn CustomNodeWriter) -> WriteResult<()> {
-        let mut temp_writer = cmark_writer::writer::CommonMarkWriter::new();
+    fn write_custom(&self, writer: &mut CommonMarkWriter) -> WriteResult<()> {
+        let mut temp_writer = CommonMarkWriter::new();
         temp_writer.write(&self.node)?;
         let content = temp_writer.into_string();
         writer.write_str(&content)?;
@@ -135,8 +172,12 @@ impl CenterNode {
         Ok(())
     }
 
-    fn is_block_custom(&self) -> bool {
-        true
+    fn write_html_custom(&self, writer: &mut HtmlWriter) -> HtmlWriteResult<()> {
+        let mut temp_writer = HtmlWriter::new();
+        temp_writer.write_node(&self.node)?;
+        let content = temp_writer.into_string();
+        writer.write_str(&content)?;
+        Ok(())
     }
 }
 
