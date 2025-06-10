04821830a96dc577c9150131487a8653ae8cd3e6
Myriad-Dreamin <camiyoru@gmail.com>
2025-06-10 17:34:30 +0800

    feat: nice typing repr

--- crates/tinymist-analysis/src/ty/scheme.rs
+++ crates/tinymist-analysis/src/ty/scheme.rs
@@ -1,43 +1,89 @@
 #![allow(unused)]
 
 use core::fmt;
+use std::{path::Path, sync::OnceLock};
 
 use comemo::Track;
 use typst::{
     engine::Engine,
-    foundations::{func::Repr, Context, Value},
+    foundations::{func::Repr, Args, Context, Func, Str, Value},
     syntax::Source,
 };
 
 use super::{term_value, Ty};
-use crate::ty::TypeInfo;
+use crate::ty::{BuiltinTy, Interned, TypeInfo};
 
 pub struct TySchemeWorker<'a> {
-    engine: Engine<'a>,
     scheme: &'a mut TypeInfo,
 }
 
 impl TySchemeWorker<'_> {
-    pub fn define(&mut self, v: &Value) -> Ty {
-        if let Value::Func(f) = v {
-            match f.inner() {
-                Repr::Element(..) | Repr::Native(..) | Repr::Plugin(..) | Repr::With(..) => {
-                    self.define_value(v)
-                }
-                Repr::Closure(..) => {
-                    let ctx = Context::default();
-                    let values = Vec::<Value>::default();
-                    let v = f.call(&mut self.engine, ctx.track(), values).unwrap();
-                    self.define_value(&v)
-                }
-            }
-        } else {
+    fn is_typing_item(&self, v: &Value) -> bool {
+        matches!(v, Value::Func(f) if f.span().id().is_some_and(|s| s.vpath().as_rooted_path() == Path::new("/typings.typ")))
+    }
+}
+
+impl TySchemeWorker<'_> {
+    pub fn define(&mut self, k: &str, v: &Value) -> Ty {
+        if !self.is_typing_item(v) {
             self.define_value(v)
+        } else {
+            self.define_typing_value(k, v)
         }
     }
 
+    fn define_typing_value(&mut self, k: &str, v: &Value) -> Ty {
+        let closure = match v {
+            Value::Func(f) => f,
+            _ => return Ty::Any,
+        };
+
+        let with = match closure.inner() {
+            Repr::With(c) => c,
+            _ => return Ty::Any,
+        };
+
+        self.define_typing(k, &with.0, with.1.clone())
+    }
+
+    fn define_typing(&mut self, k: &str, func: &Func, mut args: Args) -> Ty {
+        match func.inner() {
+            Repr::Native(..) | Repr::Element(..) | Repr::Plugin(..) => Ty::Any,
+            Repr::With(with) => {
+                args.items = with.1.items.iter().cloned().chain(args.items).collect();
+                self.define_typing(k, &with.0, args)
+            }
+            Repr::Closure(closure) => self.ty_cons(k, args).unwrap_or(Ty::Any),
+        }
+    }
+
+    fn ty_cons(&mut self, k: &str, mut args: Args) -> Option<Ty> {
+        crate::log_debug_ct!("ty_cons({k}) args: {args:?}");
+        let kind = args.named::<Str>("kind").ok()??;
+        Some(match kind.as_str() {
+            "var" => self.define(k, &args.eat::<Value>().ok()??),
+            "arr" => {
+                let ty = self.define(k, &args.eat::<Value>().ok()??);
+                Ty::Array(Interned::new(ty))
+            }
+            "tuple" => {
+                let values = args.all::<Value>().ok()?;
+                let values = values
+                    .into_iter()
+                    .map(|v| self.define(k, &v))
+                    .collect::<Vec<_>>();
+                Ty::Tuple(Interned::new(values))
+            }
+            _ => Ty::Any,
+        })
+    }
+
     fn define_value(&self, v: &Value) -> Ty {
-        term_value(v)
+        let ty = term_value(v);
+        match ty {
+            Ty::Builtin(BuiltinTy::TypeType(ty)) => Ty::Builtin(BuiltinTy::Type(ty)),
+            ty => ty,
+        }
     }
 }
 
@@ -60,37 +106,25 @@ pub mod tests {
     #[test]
     fn test_check() {
         snapshot_testing("type_schema", &|mut world, _path| {
+            let main_id = world.main();
             world
-                .map_shadow(
-                    Path::new("/typings.typ"),
+                .map_shadow_by_id(
+                    main_id.join("/typings.typ"),
                     Bytes::from_string(include_str!("typings.typ")),
                 )
                 .unwrap();
             world
-                .map_shadow(
-                    Path::new("/builtin.typ"),
+                .map_shadow_by_id(
+                    main_id.join("/builtin.typ"),
                     Bytes::from_string(include_str!("builtin.typ")),
                 )
                 .unwrap();
-            let source = world.source(world.main()).unwrap();
-            let module = typst_shim::eval::eval_compat(&world, &source).unwrap();
+            let source = world.source(main_id).unwrap();
 
-            let route = Route::default();
-            let mut sink = Sink::default();
-            let introspector = Introspector::default();
-            let traced = Traced::default();
-            let engine = Engine {
-                routines: &typst::ROUTINES,
-                world: ((&world) as &dyn World).track(),
-                introspector: introspector.track(),
-                traced: traced.track(),
-                sink: sink.track_mut(),
-                route,
-            };
+            let module = typst_shim::eval::eval_compat(&world, &source).unwrap();
 
             let mut scheme = TypeInfo::default();
             let mut w = TySchemeWorker {
-                engine,
                 scheme: &mut scheme,
             };
             for (k, v) in module.scope().iter() {
@@ -99,11 +133,11 @@ pub mod tests {
                     continue;
                 }
 
-                let ty = w.define(v.read());
+                let ty = w.define(k, v.read());
                 w.scheme.exports.insert(k.into(), ty);
             }
 
-            let result = format!("{:#?}", TypeCheckSnapshot(&source, &scheme));
+            let result = format!("{:#?}", TypeCheckSnapshot(&scheme));
 
             insta::assert_snapshot!(result);
         });
