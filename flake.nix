c63b91ebc999e22014ee1986787b76fbbe839ad6
Myriad-Dreamin <camiyoru@gmail.com>
2025-06-23 13:24:00 +0800

    dev: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+# https://wiki.nixos.org/wiki/Flakes
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
