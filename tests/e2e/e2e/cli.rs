5b2296b0676dd928b25c34d44c30741e01ef280d
Myriad-Dreamin <camiyoru@gmail.com>
2025-07-30 21:38:12 +0800

    test: passing tests

--- tests/e2e/e2e/cli.rs
+++ tests/e2e/e2e/cli.rs
@@ -20,43 +20,37 @@ fn cli_compile() {
         assert!(rel_out.is_relative(), "rel_out should be relative {rel_out:?}");
 
         // absolute INPUT, absolute OUTPUT
-        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(cwd.join(INPUT_REL)).arg(abs_out.join("test.pdf")), @r#"
+        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(cwd.join(INPUT_REL)).arg(abs_out.join("test1.pdf")), @r"
         success: true
         exit_code: 0
         ----- stdout -----
-        Output format: Pdf, at Some("/home/kamiyoru/work/rust/tinymist/target/e2e/tinymist-cli/.tmplvF4tu/test.pdf")
 
         ----- stderr -----
-        "#);
+        ");
         // absolute INPUT, relative OUTPUT
-        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(cwd.join(INPUT_REL)).arg(rel_out.join("test2.pdf")), @r#"
-        success: false
-        exit_code: 1
-        ----- stdout -----
-        Output format: Pdf, at Some("target/e2e/tinymist-cli/.tmplvF4tu/test2.pdf")
-
-        ----- stderr -----
-        Error: crates/tinymist/src/task/export.rs:182:13: ExportTask(ExportPdf(ExportPdfTask { export: ExportTask { when: Never, output: Some(PathPattern("target/e2e/tinymist-cli/.tmplvF4tu/test2.pdf")), transform: [] }, pdf_standards: [], creation_timestamp: None })): output path is relative: "target/e2e/tinymist-cli/.tmplvF4tu/test2.pdf"
-        "#);
-        // relative INPUT, absolute OUTPUT
-        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(INPUT_REL).arg(abs_out.join("test3.pdf")), @r#"
+        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(cwd.join(INPUT_REL)).arg(rel_out.join("test2.pdf")), @r"
         success: true
         exit_code: 0
         ----- stdout -----
-        Output format: Pdf, at Some("/home/kamiyoru/work/rust/tinymist/target/e2e/tinymist-cli/.tmplvF4tu/test3.pdf")
 
         ----- stderr -----
-        "#);
-        // relative INPUT, relative OUTPUT
-        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(INPUT_REL).arg(rel_out.join("test4.pdf")), @r#"
-        success: false
-        exit_code: 1
+        ");
+        // relative INPUT, absolute OUTPUT
+        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(INPUT_REL).arg(abs_out.join("test3.pdf")), @r"
+        success: true
+        exit_code: 0
         ----- stdout -----
-        Output format: Pdf, at Some("target/e2e/tinymist-cli/.tmplvF4tu/test4.pdf")
 
         ----- stderr -----
-        Error: crates/tinymist/src/task/export.rs:182:13: ExportTask(ExportPdf(ExportPdfTask { export: ExportTask { when: Never, output: Some(PathPattern("target/e2e/tinymist-cli/.tmplvF4tu/test4.pdf")), transform: [] }, pdf_standards: [], creation_timestamp: None })): output path is relative: "target/e2e/tinymist-cli/.tmplvF4tu/test4.pdf"
-        "#);
+        ");
+        // relative INPUT, relative OUTPUT
+        insta_cmd::assert_cmd_snapshot!(cli().arg("compile").arg(INPUT_REL).arg(rel_out.join("test4.pdf")), @r"
+        success: true
+        exit_code: 0
+        ----- stdout -----
+
+        ----- stderr -----
+        ");
 
         for i in 1..=4 {
             let output = rel_out.join(format!("test{}.pdf", i));
