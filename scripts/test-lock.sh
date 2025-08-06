8c7d8c1db715a844a256131822553314827b05fe
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-07 06:36:13 +0800

    test: enable unit test on linux

--- scripts/test-lock.sh
+++ scripts/test-lock.sh
@@ -1,3 +1,3 @@
 bookdir=tests/workspaces/book
-typst compile ${bookdir}/main.typ ${bookdir}/book.pdf
+# typst compile ${bookdir}/main.typ ${bookdir}/book.pdf
 cargo run --bin tinymist -- compile --lockfile ${bookdir}/tinymist.lock ${bookdir}/main.typ ${bookdir}/book.pdf
