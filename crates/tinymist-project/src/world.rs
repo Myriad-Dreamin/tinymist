df27d629a21dba3b805ea23ff987264f873943c1
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-11 07:32:31 +0800

    feat: adjust cargo toml

--- crates/tinymist-project/src/world.rs
+++ crates/tinymist-project/src/world.rs
@@ -4,8 +4,11 @@ pub use tinymist_world as base;
 pub use tinymist_world::args::*;
 pub use tinymist_world::config::CompileFontOpts;
 pub use tinymist_world::entry::*;
-pub use tinymist_world::{font, package, system, vfs};
+pub use tinymist_world::{font, package, vfs};
 pub use tinymist_world::{
     with_main, CompilerUniverse, CompilerWorld, DiagnosticFormat, EntryOpts, EntryState,
     RevisingUniverse, SourceWorld, TaskInputs,
 };
+
+#[cfg(feature = "system")]
+pub use tinymist_world::system;
