a823396aecc0d3f2eb9ee1f2528398411b8aec30
QuadnucYard <quadnucyard@gmail.com>
2025-09-15 11:22:09 +0800

    test: make e2e test work

--- editors/vscode/src/test/e2e/export.test.ts
+++ editors/vscode/src/test/e2e/export.test.ts
@@ -1,63 +1,125 @@
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
+
+import { hash } from "node:crypto";
+import * as fs from "node:fs";
 import * as vscode from "vscode";
+import type { ExportOpts } from "../../cmd.export";
+import type { ExportKind } from "../../features/export";
+import type { ExportResponse } from "../../lsp";
 import type { Context } from ".";
-import * as fs from "fs";
-import { hash } from "crypto";
 
 export async function getTests(ctx: Context) {
   const workspaceCtx = ctx.workspaceCtx("export");
   await workspaceCtx.suite("export", async (suite) => {
     // const uri = workspaceCtx.workspaceUri();
-    const uri = workspaceCtx.getWorkspace("export");
+    const baseUri = workspaceCtx.getWorkspace("export");
     vscode.window.showInformationMessage("Start export tests.");
 
-    console.log("Start all tests on ", uri.fsPath);
+    console.log("Start all tests on ", baseUri.fsPath);
 
-    suite.addTest("export by test", async () => {
-      const mainUrl = vscode.Uri.joinPath(uri, "main.typ");
+    // check and clear target directory
+    const targetDir = vscode.Uri.joinPath(baseUri, "target");
+    if (fs.existsSync(targetDir.fsPath)) {
+      fs.rmdirSync(targetDir.fsPath, { recursive: true });
+    }
+    const dirPat = /target[\\/]typst[\\/]/;
 
-      const mainTyp = await ctx.openDocument(mainUrl);
+    const prepareMain = async (mainPath: string) => {
+      const mainUrl = vscode.Uri.joinPath(baseUri, mainPath);
 
-      // check and clear target directory
-      const targetDir = vscode.Uri.joinPath(uri, "target");
-      if (fs.existsSync(targetDir.fsPath)) {
-        fs.rmdirSync(targetDir.fsPath, { recursive: true });
+      await ctx.openDocument(mainUrl);
+    };
+
+    const exportDoc = async (mainPath: string, kind: ExportKind, opts?: ExportOpts) => {
+      await prepareMain(mainPath);
+
+      return await vscode.commands.executeCommand<ExportResponse | null>(
+        "tinymist.export",
+        kind,
+        opts,
+      );
+    };
+
+    const getFileHash = (path?: string | null) => {
+      if (!path || !fs.existsSync(path)) {
+        throw new Error(`File ${path} does not exist`);
+      }
+      ctx.expect(!!dirPat.exec(path), `${path} is not under correct directory`).eq(true);
+      return hash("sha256", fs.readFileSync(path), "hex");
+    };
+
+    const expectSingleHash = (response: ExportResponse | null) => {
+      if (!response) {
+        throw new Error("No response from export command");
+      }
+      if ("items" in response) {
+        throw new Error("Expected single export, got multiple");
+      }
+      const sha256 = getFileHash(response.path);
+      return ctx.expect(sha256.slice(0, 8), `sha256:${sha256}`);
+    };
+
+    const expectPaged = (response: ExportResponse | null) => {
+      if (!response) {
+        throw new Error("No response from export command");
+      }
+      if (!("items" in response)) {
+        throw new Error("Expected multi-page export, got single");
       }
 
-      const exported: string[] = [];
-      await vscode.commands.executeCommand<string>("tinymist.exportCurrentPdf");
-      for (const kind of ["Pdf", "Png", "Svg", "Html", "Markdown", "Text"] as const) {
-        const outputPath = await vscode.commands.executeCommand<string>("tinymist.export", kind);
-        exported.push(outputPath);
-      }
+      return ctx.expect(
+        response.items.map((item) => ({
+          page: item.page,
+          hash: getFileHash(item.path).slice(0, 8),
+        })),
+      );
+    };
 
-      const dirPat = /target[\\/]typst[\\/]/;
-      for (const path of exported) {
-        if (!path) {
-          throw new Error(`Failed to export ${exported}`);
-        }
+    suite.addTest("export current pdf", async () => {
+      await prepareMain("main.typ");
 
-        ctx.expect(!!dirPat.exec(path), `${path} is not under correct directory`).eq(true);
-        ctx.expect(fs.existsSync(path), `${path} does not exist`).eq(true);
-
-        if (path.endsWith(".png")) {
-          const sha256 = hash("sha256", fs.readFileSync(path), "hex");
-          ctx
-            .expect(sha256, `sha256:${sha256}`)
-            .eq("4523673a2ab4ce07de888830e3a84c2e70529703d904ac38138cab904a15dca8");
-        }
-
-        if (path.endsWith(".txt")) {
-          const content = fs.readFileSync(path, "utf-8");
-          ctx
-            .expect(content)
-            .eq(`A Hello World Example of Export Typst Document to Various FormatsHello World.`);
-        }
-      }
-
-      // close the editor
-      await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+      const resp = await vscode.commands.executeCommand<ExportResponse | null>(
+        "tinymist.exportCurrentPdf",
+      );
+      expectSingleHash(resp).to.be.a("string");
     });
+
+    suite.addTest("export pdf", async () => {
+      const resp = await exportDoc("main.typ", "Pdf", { creationTimestamp: "0" });
+      expectSingleHash(resp).eq("f5d1a181");
+    });
+
+    suite.addTest("export html", async () => {
+      const resp = await exportDoc("main.typ", "Html");
+      expectSingleHash(resp).eq("a55cf03e");
+    });
+
+    suite.addTest("export markdown", async () => {
+      const resp = await exportDoc("main.typ", "Markdown");
+      expectSingleHash(resp).eq("62ca0c72");
+    });
+
+    suite.addTest("export tex", async () => {
+      const resp = await exportDoc("main.typ", "TeX");
+      expectSingleHash(resp).eq("492c3e62");
+    });
+
+    suite.addTest("export text", async () => {
+      const resp = await exportDoc("main.typ", "Text");
+      expectSingleHash(resp).eq("8ae8f637");
+    });
+
+    suite.addTest("export query", async () => {
+      const resp = await exportDoc("main.typ", "Query", { format: "json", selector: "heading" });
+      expectSingleHash(resp).eq("a08f208d");
+    });
+
+    /* suite.addTest("export png", async () => {
+      const resp = await exportDoc("main.typ", "Png");
+      expectSingleHash(resp).eq("8ae8f637");
+
+      // expectPaged(resp).eq([{ page: 1, hash: "4523673a" }]);
+    });*/
   });
 }
