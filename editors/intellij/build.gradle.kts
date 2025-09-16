a7e237e4a8eae8053a36b6a6442cd9cb7539e2aa
Julius Schmitt <git@jsch.in>
2025-09-16 15:11:04 +0200

    fix gradlew

--- editors/intellij/build.gradle.kts
+++ editors/intellij/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdeaCommunity("2025.1")
-        // todo: intellijIdeaUltimate("2025.1")
         testFramework(TestFrameworkType.Platform)
         plugins(listOf("com.redhat.devtools.lsp4ij:0.15.1-20250829-070912@nightly"))
     }
