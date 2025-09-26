34bc5164664540b54e7543736f4e0b45fb913155
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2025-09-26 15:58:50 +0800

    build: bump version to 0.13.28-rc1 (#2134)
    +tag v0.13.28-rc1

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -144,7 +144,7 @@ const newCargoToml = cargoToml.replace(
   `tinymist-assets = { version = "=${tag}" }`,
 );
 await fs.writeFile("Cargo.toml", newCargoToml);
-await spawn("upate-lock", "cargo update tinymist");
+await spawn("update-lock", "cargo update tinymist");
 // sleep 20 seconds to wait for cargo.lock to be updated
 await new Promise((resolve) => setTimeout(resolve, 20000));
 
