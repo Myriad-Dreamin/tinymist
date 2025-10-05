74f7d05cd8d481140ea6a8dd3ce9883eb9e5deec
ParaN3xus <136563585+ParaN3xus@users.noreply.github.com>
2025-10-05 16:59:54 +0800

    dev: update nightly (#2149)
    Update typst to
    https://github.com/typst/typst/commit/b14c08e97a50178617c4ed46eb01445475ac6ac6

--- tests/e2e/e2e/lsp.rs
+++ tests/e2e/e2e/lsp.rs
@@ -22,7 +22,7 @@ fn test_lsp() {
         });
 
         let hash = replay_log(&root.join("neovim"));
-        insta::assert_snapshot!(hash, @"siphash128_13:f9ba12e829fe6e5cb16d16ef310c4643");
+        insta::assert_snapshot!(hash, @"siphash128_13:37494c30b1b74469233b09f063b139a8");
     }
 
     {
@@ -33,7 +33,7 @@ fn test_lsp() {
         });
 
         let hash = replay_log(&root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:7eced41bc62b008708a200ec68ca4cb4");
+        insta::assert_snapshot!(hash, @"siphash128_13:38d2e27c6858270675f5752d89040852");
     }
 }
 
