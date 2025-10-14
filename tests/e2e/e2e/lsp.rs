25d622aba05bf0a89a49da31e85cd2479e0bb7d0
Myriad-Dreamin <camiyoru@gmail.com>
2025-10-15 01:50:16 +0800

    test: update snapshot

--- tests/e2e/e2e/lsp.rs
+++ tests/e2e/e2e/lsp.rs
@@ -33,7 +33,7 @@ fn test_lsp() {
         });
 
         let hash = replay_log(&root.join("vscode"));
-        insta::assert_snapshot!(hash, @"siphash128_13:5019cb39687d1a16853bc80320d00e26");
+        insta::assert_snapshot!(hash, @"siphash128_13:37838db38884e581e48751286df407d0");
     }
 }
 
