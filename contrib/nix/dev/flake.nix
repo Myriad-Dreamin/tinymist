4e65233b33a4291d17d5abd08aba014c1966f942
Myriad-Dreamin <camiyoru@gmail.com>
2025-10-27 13:55:22 +0800

    build: bump vesrion to 0.13.30

--- contrib/nix/dev/flake.nix
+++ contrib/nix/dev/flake.nix
@@ -26,7 +26,7 @@
           pname = "tinymist";
           # Please update the corresponding vscode extension when updating
           # this derivation.
-          version = "0.13.30-rc2";
+          version = "0.13.30";
 
           src = pkgs.lib.cleanSource ../../..;
 
