e23530b96e046afa4dfe52b897db5feeb3ae1c9e
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2025-11-02 04:47:10 +0800

    build: bump version to 0.14.0-rc2 (#2213)
    +tag v0.14.0-rc2

--- contrib/nix/dev/flake.nix
+++ contrib/nix/dev/flake.nix
@@ -26,7 +26,7 @@
           pname = "tinymist";
           # Please update the corresponding vscode extension when updating
           # this derivation.
-          version = "0.14.0-rc1";
+          version = "0.14.0-rc2";
 
           src = pkgs.lib.cleanSource ../../..;
 
