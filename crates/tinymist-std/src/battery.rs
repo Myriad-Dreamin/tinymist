1ef7ed5cb2eaa78d9408d1d7374991f3f7ae4640
Myriad-Dreamin <camiyoru@gmail.com>
2025-11-16 08:54:15 +0800

    docs: update power save checks

--- crates/tinymist-std/src/battery.rs
+++ crates/tinymist-std/src/battery.rs
@@ -6,12 +6,13 @@ mod system {
     pub fn is_power_saving() -> bool {
         ::battery::Manager::new()
             .ok()
-            .and_then(|manager| manager.batteries().ok())
-            .map(|mut batteries| {
-                batteries.any(|battery| match battery {
-                    Ok(bat) => matches!(bat.state(), ::battery::State::Discharging),
-                    Err(_) => false,
-                })
+            .and_then(|manager| (manager.batteries().ok().zip(manager.batteries().ok())))
+            .map(|(mut batteries_first, mut batteries)| {
+                batteries_first.next().is_some()
+                    && batteries.all(|battery| match battery {
+                        Ok(bat) => matches!(bat.state(), ::battery::State::Discharging),
+                        Err(_) => false,
+                    })
             })
             .unwrap_or(false)
     }
