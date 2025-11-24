934c1a5369df42e41f255ffc22758d70bb87a76f
Myriad-Dreamin <camiyoru@gmail.com>
2025-11-25 02:59:10 +0800

    build: bump version to 0.14.4-rc1

--- contrib/nix/dev/flake.nix
+++ contrib/nix/dev/flake.nix
@@ -26,7 +26,7 @@
           pname = "tinymist";
           # Please update the corresponding vscode extension when updating
           # this derivation.
-          version = "0.14.2";
+          version = "0.14.4-rc1";
 
           src = pkgs.lib.cleanSource ../../..;
 
