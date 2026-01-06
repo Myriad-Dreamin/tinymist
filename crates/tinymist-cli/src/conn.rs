999dde6c9f280773621e06622e41a972fd8aea42
Myriad-Dreamin <camiyoru@gmail.com>
2026-01-06 21:04:13 +0800

    dev: finish notification

--- crates/tinymist-cli/src/conn.rs
+++ crates/tinymist-cli/src/conn.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
 
 use parking_lot::Mutex;
@@ -20,7 +20,14 @@ pub fn client_root<M: TryFrom<Message, Error = anyhow::Error> + GetMessageKind>(
 }
 
 #[derive(Default)]
-struct TinymistHook(Mutex<FxHashMap<RequestId, typst_timing::TimingScope>>);
+struct TinymistHook {
+    /// Data for stalling tracking.
+    may_stall: Mutex<VecDeque<(RequestId, tinymist_std::time::Time)>>,
+    /// Whether finished for stalling tracking.
+    stall_data: Mutex<FxHashMap<RequestId, StallTab>>,
+    /// Data for performance tracking.
+    perf: Mutex<FxHashMap<RequestId, typst_timing::TimingScope>>,
+}
 
 impl fmt::Debug for TinymistHook {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -30,10 +37,10 @@ impl fmt::Debug for TinymistHook {
 
 impl LsHook for TinymistHook {
     fn start_request(&self, req_id: &RequestId, method: &str) {
-        ().start_request(req_id, method);
+        self.start_stall(req_id.clone(), method);
 
         if let Some(scope) = typst_timing::TimingScope::new(static_str(method)) {
-            let mut map = self.0.lock();
+            let mut map = self.perf.lock();
             map.insert(req_id.clone(), scope);
         }
     }
@@ -41,12 +48,12 @@ impl LsHook for TinymistHook {
     fn stop_request(
         &self,
         req_id: &RequestId,
-        method: &str,
-        received_at: tinymist_std::time::Instant,
+        _method: &str,
+        _received_at: tinymist_std::time::Instant,
     ) {
-        ().stop_request(req_id, method, received_at);
+        self.stop_stall(req_id.clone());
 
-        if let Some(scope) = self.0.lock().remove(req_id) {
+        if let Some(scope) = self.perf.lock().remove(req_id) {
             let _ = scope;
         }
     }
@@ -65,6 +72,73 @@ impl LsHook for TinymistHook {
     }
 }
 
+impl TinymistHook {
+    fn start_stall(&self, msg_id: RequestId, method: &str) {
+        let mut may_stall = self.may_stall.lock();
+        let time = tinymist_std::time::now();
+        may_stall.push_back((msg_id.clone(), time));
+        self.stall_data.lock().insert(
+            msg_id,
+            StallTab {
+                method: static_str(method),
+                stalled: false,
+            },
+        );
+
+        while !may_stall.is_empty() {
+            // consume one anyway.
+            let Some((id, since)) = may_stall.pop_front() else {
+                break;
+            };
+
+            let elapsed = match time.duration_since(since) {
+                Ok(elapsed) => elapsed,
+                Err(err) => {
+                    log::error!("failed to get elapsed time for stall tracking: {err}");
+                    break;
+                }
+            };
+
+            if elapsed.as_secs() > 10 {
+                let mut stall_data = self.stall_data.lock();
+                let Some(tab) = stall_data.get_mut(&id) else {
+                    continue;
+                };
+                log::warn!(
+                    "stall detected: {id:?}, method: {:?}, since: {since:?}, elapsed: {elapsed:?}",
+                    tab.method
+                );
+                tab.stalled = true;
+            } else {
+                // This is free, because vecqueue is a ring buffer.
+                may_stall.push_back((id, since));
+                break;
+            }
+        }
+    }
+
+    fn stop_stall(&self, msg_id: RequestId) {
+        let mut _may_stall = self.may_stall.lock();
+        let result = self.stall_data.lock().remove(&msg_id);
+        if let Some(tab) = result
+            && tab.stalled
+        {
+            log::info!(
+                "stalling request {msg_id:?} finished, method: {:?}",
+                tab.method
+            );
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct StallTab {
+    /// The method of the request to detect the stall.
+    method: &'static str,
+    /// Whether the request is stalled.
+    stalled: bool,
+}
+
 fn static_str(s: &str) -> &'static str {
     static STRS: Mutex<FxHashMap<EcoString, &'static str>> =
         Mutex::new(HashMap::with_hasher(FxBuildHasher));
@@ -75,6 +149,6 @@ fn static_str(s: &str) -> &'static str {
     }
 
     let static_ref: &'static str = String::from(s).leak();
-    strs.insert(static_ref.into(), static_ref);
+    strs.insert(s.into(), static_ref);
     static_ref
 }
