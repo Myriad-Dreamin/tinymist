7f1a653dd6c495c3566a89898a4d64a4ad115a47
Myriad-Dreamin <camiyoru@gmail.com>
2026-01-09 01:27:50 +0800

    fix: only lint source files

--- crates/tinymist-query/src/diagnostics.rs
+++ crates/tinymist-query/src/diagnostics.rs
@@ -53,7 +53,9 @@ impl<'w> DiagWorker<'w> {
         let source = self.source;
         self.source = "tinymist-lint";
         for dep in self.ctx.world().depended_files() {
-            if WorkspaceResolver::is_package_file(dep) {
+            if WorkspaceResolver::is_package_file(dep)
+                || dep.vpath().as_rooted_path().extension().unwrap_or_default() != "typ"
+            {
                 continue;
             }
 
