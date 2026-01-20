Myriad-Dreamin/tinymist

commit c544da81b5e5a92f66f936b4793e965cd9392317

feat: PDF export for markdown documents (#2241)
Now `tinymist.exportPdf` accepts markdown files. this feature is
experimental so this is disabled by default:

```jsonc
{
   "tinymist.convertExtension": ["markdown"],
}
```

`tinymist.exportPdf` uses a processor (typst package) to convert
markdown documents. for example `@preview/cmarker:0.1.6`:
```
#import "@preview/cmarker:0.1.6": render
#render(markdown-content)
```

The user could specify a custom processor. For example, rendering the
markdown document for mobiles:
```json
{
  "tinymist.convertExtension": [
    {
      "language": "markdown",
      "processor": "@local/md-to-typst:0.1.0"
    }
  ]
}
```

Sample implementation of a processor:
```typ
#import "@preview/cmarker:0.1.6": render as cmarker-render

#let render(body) = {
  set page(width: 400pt, height: auto)
  set par(justify: true)
  cmarker-render(body)
}
```

Description: this processor adds additional styles when rendering a
markdown document to PDF.

crates/tinymist/src/cmd/export.rs
@@ -30,6 +30,8 @@ struct ExportPdfOpts {
     creation_timestamp: Option<String>,
     /// A PDF standard that Typst can enforce conformance with.
     pdf_standard: Option<Vec<PdfStandard>>,
+    /// The processor package to use for the export (only for Markdown).
+    processor: Option<String>,
     /// By default, even when not producing a `PDF/UA-1` document, a tagged PDF
     /// document is written to provide a baseline of accessibility. In some
     /// circumstances (for example when trying to reduce the size of a document)
@@ -93,6 +95,7 @@ struct ExportActionOpts {
 impl ServerState {
     /// Export the current document as PDF file(s).
     pub fn export_pdf(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportPdfOpts);
 
         let creation_timestamp = if let Some(value) = opts.creation_timestamp {
@@ -104,19 +107,24 @@ impl ServerState {
             self.config.creation_timestamp()
         };
         let no_pdf_tags = opts.no_pdf_tags.unwrap_or(self.config.no_pdf_tags());
-        let pdf_standards = opts.pdf_standard.or_else(|| self.config.pdf_standards());
-
+        let pdf_standards = opts
+            .pdf_standard
+            .or_else(|| self.config.pdf_standards())
+            .unwrap_or_default();
         let export = self.config.export_task();
-        self.export(
-            ProjectTask::ExportPdf(ExportPdfTask {
-                export,
-                pages: opts.pages,
-                pdf_standards: pdf_standards.unwrap_or_default(),
-                no_pdf_tags,
-                creation_timestamp,
-            }),
-            args,
-        )
+        let task = ProjectTask::ExportPdf(ExportPdfTask {
+            export,
+            pages: opts.pages,
+            pdf_standards,
+            no_pdf_tags,
+            creation_timestamp,
+        });
+
+        if path.extension().and_then(|ext| ext.to_str()) == Some("md") {
+            self.export_md(path, opts.processor, task, args)
+        } else {
+            self.export(task, args)
+        }
     }
 
     /// Export the current document as HTML file(s).
@@ -234,5 +242,20 @@ impl ServerState {
         let open = action_opts.open;
 
         run_query!(self.OnExport(path, task, write, open))
+    }
+
+    /// Exports the a markdown document using a custom template.
+    pub fn export_md(
+        &mut self,
+        path: PathBuf,
+        processor: Option<String>,
+        task: ProjectTask,
+        mut args: Vec<JsonValue>,
+    ) -> ScheduleResult {
+        let action_opts = get_arg_or_default!(args[2] as ExportActionOpts);
+        let write = action_opts.write.unwrap_or(true);
+        let open = action_opts.open;
+
+        run_query!(self.OnExportMd(path, processor, task, write, open))
     }
 }

crates/tinymist/src/task/export.rs
@@ -6,21 +6,28 @@ use std::sync::{Arc, OnceLock};
 use std::{ops::DerefMut, pin::Pin};
 
 use reflexo::ImmutPath;
-use reflexo_typst::{Bytes, CompilationTask, ExportComputation};
-use sync_ls::{internal_error, just_future};
+use reflexo_typst::{
+    Bytes, CompilationTask, CompileSnapshot, ExportComputation, ShadowApi, WorldComputeGraph,
+};
+use sync_ls::{internal_error, invalid_params, just_future, LspResult};
+use tinymist_project::diag::print_diagnostics_to_string;
 use tinymist_project::LspWorld;
-use tinymist_query::{OnExportRequest, OnExportResponse, PagedExportResponse, GLOBAL_STATS};
+use tinymist_query::{
+    CompilerQueryResponse, OnExportMdRequest, OnExportRequest, OnExportResponse,
+    PagedExportResponse, GLOBAL_STATS,
+};
 use tinymist_std::error::prelude::*;
 use tinymist_std::fs::paths::write_atomic;
 use tinymist_std::path::PathClean;
 use tinymist_std::typst::TypstDocument;
 use tinymist_task::{
     output_template, DocumentQuery, ExportMarkdownTask, ExportPngTask, ExportSvgTask, ExportTarget,
-    ImageOutput, PdfExport, PngExport, SvgExport, TextExport,
+    ImageOutput, PathPattern, PdfExport, PngExport, SvgExport, TextExport,
 };
 use tokio::sync::mpsc;
 use typlite::{Format, Typlite};
 use typst::ecow::EcoString;
+use typst::foundations::Repr;
 
 use futures::Future;
 use parking_lot::Mutex;
@@ -48,22 +55,24 @@ impl ServerState {
             write,
         } = req;
         let entry = self.entry_resolver().resolve(Some(path.as_path().into()));
+
         let lock_dir = self.entry_resolver().resolve_lock(&entry);
-
         let update_dep = lock_dir.clone().map(|lock_dir| {
-            |snap: LspComputeGraph| async move {
-                let mut updater = update_lock(lock_dir.clone());
-                let world = snap.world();
-                // todo: rootless.
-                let root_dir = world.entry_state().root()?;
-                let doc_id = updater.compiled(world, (&root_dir, &lock_dir))?;
+            |snap: LspComputeGraph| {
+                tokio::spawn(async move {
+                    let mut updater = update_lock(lock_dir.clone());
+                    let world = snap.world();
+                    // todo: rootless.
+                    let root_dir = world.entry_state().root()?;
+                    let doc_id = updater.compiled(world, (&root_dir, &lock_dir))?;
 
-                updater.update_materials(doc_id.clone(), world.depended_fs_paths());
-                updater.route(doc_id, PROJECT_ROUTE_USER_ACTION_PRIORITY);
+                    updater.update_materials(doc_id.clone(), world.depended_fs_paths());
+                    updater.route(doc_id, PROJECT_ROUTE_USER_ACTION_PRIORITY);
 
-                updater.commit();
+                    updater.commit();
 
-                Some(())
+                    Some(())
+                });
             }
         });
 
@@ -77,51 +86,122 @@ impl ServerState {
             let id = snap.world().main_id();
             let _guard = GLOBAL_STATS.stat(id, "export");
 
-            let is_html = matches!(task, ProjectTask::ExportHtml { .. });
-            // todo: we may get some file missing errors here
-            let artifact = CompiledArtifact::from_graph(snap.clone(), is_html);
+            Self::on_export_typ(task, snap, write, open, update_dep).await
+        })
+    }
 
-            let res = if write {
-                // Export to file and return path
-                ExportTask::do_export(task, artifact, lock_dir)
-                    .await
-                    .map_err(internal_error)?
-            } else {
-                // Export to memory and return base64-encoded data
-                ExportTask::do_export_to_memory(task, artifact)
-                    .await
-                    .map_err(internal_error)?
-            };
+    /// Exports the current document using a custom template.
+    pub fn on_export_md(&mut self, req: OnExportMdRequest) -> QueryFuture {
+        let OnExportMdRequest {
+            path,
+            processor,
+            mut task,
+            open,
+            write,
+        } = req;
 
-            if let Some(update_dep) = update_dep {
-                tokio::spawn(update_dep(snap));
-            }
+        // Pre-substitute the output path
+        let origin_entry = self.entry_resolver().resolve(Some(path.as_path().into()));
+        let subst = task
+            .as_export()
+            .and_then(|e| e.output.as_ref().and_then(|o| o.substitute(&origin_entry)));
+        if let Some(export) = task.as_export_mut() {
+            // todo: to string lossy?
+            export.output = subst.map(|s| PathPattern::new(&s.as_os_str().to_string_lossy()));
+        }
 
-            // Only open the first page if multiple pages are exported
-            if open {
-                match &res {
-                    Some(OnExportResponse::Single {
-                        path: Some(path), ..
-                    }) => {
-                        open_external(path);
-                    }
-                    Some(OnExportResponse::Paged { items, .. }) => {
-                        if let Some(first_page) = items.first() {
-                            if let Some(path) = &first_page.path {
-                                open_external(path);
-                            }
+        let entry = origin_entry.select_in_workspace(Path::new("/__md_main.typ"));
+        let md_content = self
+            .memory_changes
+            .get(path.as_path())
+            .map(|s| Ok(s.text().to_owned()))
+            .unwrap_or_else(|| tinymist_std::fs::paths::read(&path))
+            .context("failed to read markdown file")
+            .map_err(invalid_params)?;
+
+        let snap = self.snapshot().map_err(internal_error)?;
+        just_future(async move {
+            let id = entry
+                .main()
+                .ok_or_else(|| internal_error("failed to get entry main file for md export"))?;
+            let _guard = GLOBAL_STATS.stat(Some(id), "export");
+
+            let mut world = snap.world().task(TaskInputs {
+                entry: Some(entry),
+                ..TaskInputs::default()
+            });
+            // todo: bad performance
+            world.take_db();
+
+            let processor = processor.as_deref().unwrap_or("@preview/cmarker:0.1.6");
+            log::info!("ExportPdfTask: using processor {processor:?}");
+            let content = format!(
+                r#"#import {processor:?}: render
+#render({})"#,
+                md_content.repr()
+            );
+            world
+                .map_shadow_by_id(id, Bytes::from_string(content))
+                .map_err(internal_error)?;
+
+            let snap = WorldComputeGraph::new(CompileSnapshot::from_world(world));
+
+            Self::on_export_typ(task, snap, write, open, None::<fn(LspComputeGraph)>).await
+        })
+    }
+
+    async fn on_export_typ(
+        task: ProjectTask,
+        snap: LspComputeGraph,
+        write: bool,
+        open: bool,
+        update_dep: Option<impl FnOnce(LspComputeGraph)>,
+    ) -> LspResult<CompilerQueryResponse> {
+        let is_html = matches!(task, ProjectTask::ExportHtml { .. });
+        // todo: we may get some file missing errors here
+        let artifact = CompiledArtifact::from_graph(snap.clone(), is_html);
+        let id = artifact.world().main_id();
+
+        let res = if write {
+            // Export to file and return path
+            ExportTask::do_export(task, artifact, None)
+                .await
+                .map_err(internal_error)?
+        } else {
+            // Export to memory and return base64-encoded data
+            ExportTask::do_export_to_memory(task, artifact)
+                .await
+                .map_err(internal_error)?
+        };
+
+        if let Some(update_dep) = update_dep {
+            update_dep(snap);
+        }
+
+        // Only open the first page if multiple pages are exported
+        if open {
+            match &res {
+                Some(OnExportResponse::Single {
+                    path: Some(path), ..
+                }) => {
+                    open_external(path);
+                }
+                Some(OnExportResponse::Paged { items, .. }) => {
+                    if let Some(first_page) = items.first() {
+                        if let Some(path) = &first_page.path {
+                            open_external(path);
                         }
                     }
-                    None => {
-                        log::warn!("CompileActor: on export end: no export result to open");
-                    }
-                    _ => {}
                 }
+                None => {
+                    log::warn!("CompileActor: on export end: no export result to open");
+                }
+                _ => {}
             }
+        }
 
-            log::trace!("CompileActor: on export end: {path:?} as {res:?}");
-            Ok(tinymist_query::CompilerQueryResponse::OnExport(res))
-        })
+        log::trace!("CompileActor: on export end: {id:?} as {res:?}");
+        Ok(tinymist_query::CompilerQueryResponse::OnExport(res))
     }
 }
 
@@ -476,10 +556,24 @@ impl ExportTask {
         use reflexo_vec2svg::DefaultExportFeature;
         use ProjectTask::*;
 
-        let CompiledArtifact { graph, doc, .. } = artifact;
+        let CompiledArtifact {
+            graph, doc, diag, ..
+        } = artifact;
 
         // Prepare the document.
-        let doc = doc.context("cannot export with compilation errors")?;
+        let doc = match doc {
+            Some(doc) => doc,
+            None => {
+                let diag = diag.diagnostics();
+                let error = print_diagnostics_to_string(
+                    graph.world(),
+                    diag,
+                    reflexo_typst::DiagnosticFormat::Human,
+                )
+                .unwrap_or_else(|e| e);
+                bail!("ExportTask({export_id}): document is not available for export: {error:?}")
+            }
+        };
 
         // Prepare data.
         let data = FutureFolder::compute(move |_| -> Result<ExportArtifact> {

crates/sync-lsp/src/server/dap_srv.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "system")]
 use std::sync::atomic::Ordering;
 
 use dapts::IRequest;

crates/tinymist-query/src/lib.rs
@@ -140,6 +140,21 @@ mod polymorphic {
         pub open: bool,
     }
 
+    /// A request to run an export markdown task.
+    #[derive(Debug, Clone)]
+    pub struct OnExportMdRequest {
+        /// The path of the document to export.
+        pub path: PathBuf,
+        /// The processor package to use for the export.
+        pub processor: Option<String>,
+        /// The export task to run.
+        pub task: ProjectTask,
+        /// Whether to write to file.
+        pub write: bool,
+        /// Whether to open the exported file(s) after the export is done.
+        pub open: bool,
+    }
+
     /// The response to an export request.
     #[derive(Debug, Clone, Serialize, Deserialize)]
     #[serde(untagged, rename_all = "camelCase")]
@@ -215,6 +230,8 @@ mod polymorphic {
     pub enum CompilerQueryRequest {
         /// A request to run an export task.
         OnExport(OnExportRequest),
+        /// A request to run an export markdown task.
+        OnExportMd(OnExportMdRequest),
         /// A request to get the hover information.
         Hover(HoverRequest),
         /// A request to go to the definition.
@@ -281,6 +298,7 @@ mod polymorphic {
             use FoldRequestFeature::*;
             match self {
                 Self::OnExport(..) => Mergeable,
+                Self::OnExportMd(..) => Mergeable,
                 Self::Hover(..) => PinnedFirst,
                 Self::GotoDefinition(..) => PinnedFirst,
                 Self::GotoDeclaration(..) => PinnedFirst,
@@ -318,6 +336,7 @@ mod polymorphic {
         pub fn associated_path(&self) -> Option<&Path> {
             Some(match self {
                 Self::OnExport(..) => return None,
+                Self::OnExportMd(..) => return None,
                 Self::Hover(req) => &req.path,
                 Self::GotoDefinition(req) => &req.path,
                 Self::GotoDeclaration(req) => &req.path,

crates/tinymist/src/lsp/query.rs
@@ -233,8 +233,12 @@ impl ServerState {
             ColorPresentation(req) => CompilerQueryResponse::ColorPresentation(req.request()),
             #[cfg(feature = "export")]
             OnExport(req) => return self.on_export(req),
+            #[cfg(feature = "export")]
+            OnExportMd(req) => return self.on_export_md(req),
             #[cfg(not(feature = "export"))]
             OnExport(_req) => return Err(internal_error("export feature is not enabled")),
+            #[cfg(not(feature = "export"))]
+            OnExportMd(_req) => return Err(internal_error("export feature is not enabled")),
             ServerInfo(_) => return self.collect_server_info(),
             // todo: query on dedicate projects
             _ => return self.query_on(query),

editors/vscode/src/cmd.export.ts
@@ -1,6 +1,7 @@
 export interface ExportPdfOpts {
   pages?: string[];
   creationTimestamp?: string | null;
+  processor?: string;
   pdfStandard?: string[];
   noPdfTags?: boolean;
 }

editors/vscode/src/extension.shared.ts
@@ -103,6 +103,29 @@ export async function tinymistActivate(
       vscode.commands.executeCommand("setContext", "ext.tinymistActivated", false);
     },
   });
+  // Sets a global context key to indicate that the navigate Md to Pdf icon is enabled
+  const enableNavigateMdToPdfIcon =
+    config.convertExtension instanceof Array &&
+    config.convertExtension.some(
+      (item: any) =>
+        item === "markdown" ||
+        (typeof item === "object" &&
+          item.language === "markdown" &&
+          item.showNavigationIcon !== false),
+    );
+  console.log("enableNavigateMdToPdfIcon:", config.convertExtension, enableNavigateMdToPdfIcon);
+  vscode.commands.executeCommand(
+    "setContext",
+    "ext.navigateMdToPdfIcon",
+    enableNavigateMdToPdfIcon,
+  );
+  context.subscriptions.push({
+    dispose: () => {
+      if (enableNavigateMdToPdfIcon) {
+        vscode.commands.executeCommand("setContext", "ext.navigateMdToPdfIcon", false);
+      }
+    },
+  });
 
   configureEditorAndLanguage(context, trait);
 

editors/vscode/src/features/export.ts
@@ -259,7 +259,21 @@ export async function commandShow(kind: ExportKind, extraOpts?: ExportOpts): Pro
     actionOpts.open = true;
   }
 
-  // only create pdf if it does not exist yet
+  // Gets the template from the convertExtension configuration
+  const language = activeEditor.document.languageId;
+  if (language === "markdown") {
+    const convertExtension = conf.get("convertExtension");
+    if (convertExtension && convertExtension instanceof Array) {
+      const convertExtensionItem = convertExtension.find((item: any) => item.language === language);
+      if (convertExtensionItem && convertExtensionItem.processor) {
+        extraOpts ||= {};
+        // todo: processor type
+        (extraOpts as ExportPdfOpts).processor = convertExtensionItem.processor;
+      }
+    }
+  }
+
+  // only creates pdf if it does not exist yet
   const exportResponse = await commandExport(kind, extraOpts, actionOpts);
   if (!exportResponse || "message" in exportResponse) {
     // show error message

commit c20de42f9fe7ff5a0ed63157198dd1a6b1cf1659

fix: don't parsed path twice (#2363)
Since `get_arg` takes ownership of json values, `export` failed to get
first path argument when the it is called from `export_pdf`. This bug
was introduced by #2241

crates/tinymist/src/cmd/export.rs
@@ -123,22 +123,29 @@ impl ServerState {
         if path.extension().and_then(|ext| ext.to_str()) == Some("md") {
             self.export_md(path, opts.processor, task, args)
         } else {
-            self.export(task, args)
+            self.export(path, task, args)
         }
     }
 
     /// Export the current document as HTML file(s).
     pub fn export_html(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let _opts = get_arg_or_default!(args[1] as ExportOpts);
         let export = self.config.export_task();
-        self.export(ProjectTask::ExportHtml(ExportHtmlTask { export }), args)
+        self.export(
+            path,
+            ProjectTask::ExportHtml(ExportHtmlTask { export }),
+            args,
+        )
     }
 
     /// Export the current document as Markdown file(s).
     pub fn export_markdown(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportTypliteOpts);
         let export = self.config.export_task();
         self.export(
+            path,
             ProjectTask::ExportMd(ExportMarkdownTask {
                 processor: opts.processor,
                 assets_path: opts.assets_path,
@@ -150,9 +157,11 @@ impl ServerState {
 
     /// Export the current document as Tex file(s).
     pub fn export_tex(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportTypliteOpts);
         let export = self.config.export_task();
         self.export(
+            path,
             ProjectTask::ExportTeX(ExportTeXTask {
                 processor: opts.processor,
                 assets_path: opts.assets_path,
@@ -164,13 +173,19 @@ impl ServerState {
 
     /// Export the current document as Text file(s).
     pub fn export_text(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let _opts = get_arg_or_default!(args[1] as ExportOpts);
         let export = self.config.export_task();
-        self.export(ProjectTask::ExportText(ExportTextTask { export }), args)
+        self.export(
+            path,
+            ProjectTask::ExportText(ExportTextTask { export }),
+            args,
+        )
     }
 
     /// Query the current document and export the result as JSON file(s).
     pub fn export_query(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportQueryOpts);
         // todo: deprecate it
         let _ = opts.strict;
@@ -181,6 +196,7 @@ impl ServerState {
         }
 
         self.export(
+            path,
             ProjectTask::Query(QueryTask {
                 format: opts.format,
                 output_extension: opts.output_extension,
@@ -195,10 +211,12 @@ impl ServerState {
 
     /// Export the current document as Svg file(s).
     pub fn export_svg(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportSvgOpts);
 
         let export = self.config.export_task();
         self.export(
+            path,
             ProjectTask::ExportSvg(ExportSvgTask {
                 export,
                 pages: opts.pages,
@@ -211,6 +229,7 @@ impl ServerState {
 
     /// Export the current document as Png file(s).
     pub fn export_png(&mut self, mut args: Vec<JsonValue>) -> ScheduleResult {
+        let path = get_arg!(args[0] as PathBuf);
         let opts = get_arg_or_default!(args[1] as ExportPngOpts);
 
         let ppi = opts.ppi.or_else(|| self.config.ppi()).unwrap_or(144.);
@@ -221,6 +240,7 @@ impl ServerState {
 
         let export = self.config.export_task();
         self.export(
+            path,
             ProjectTask::ExportPng(ExportPngTask {
                 export,
                 pages: opts.pages,
@@ -235,8 +255,12 @@ impl ServerState {
 
     /// Export the current document as some format. The client is responsible
     /// for passing the correct absolute path of typst document.
-    pub fn export(&mut self, task: ProjectTask, mut args: Vec<JsonValue>) -> ScheduleResult {
-        let path = get_arg!(args[0] as PathBuf);
+    pub fn export(
+        &mut self,
+        path: PathBuf,
+        task: ProjectTask,
+        mut args: Vec<JsonValue>,
+    ) -> ScheduleResult {
         let action_opts = get_arg_or_default!(args[2] as ExportActionOpts);
         let write = action_opts.write.unwrap_or(true);
         let open = action_opts.open;