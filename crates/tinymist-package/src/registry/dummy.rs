33a9f725a70a3cf19b287a8163bd24546541cbe9
QuadnucYard <quadnucyard@gmail.com>
2026-01-31 20:55:24 +0800

    fix: incorrect feature configuration for separate compilation

--- crates/tinymist-package/src/registry/dummy.rs
+++ crates/tinymist-package/src/registry/dummy.rs
@@ -2,11 +2,20 @@
 
 use std::{path::Path, sync::Arc};
 
+use tinymist_std::ImmutPath;
+
 use super::{PackageError, PackageRegistry, PackageSpec};
 
 /// Dummy package registry that always returns a `NotFound` error.
 #[derive(Default, Debug)]
 pub struct DummyRegistry;
+
+impl DummyRegistry {
+    /// Get data & cache dir (empty)
+    pub fn paths(&self) -> Vec<ImmutPath> {
+        vec![]
+    }
+}
 
 impl PackageRegistry for DummyRegistry {
     fn resolve(&self, spec: &PackageSpec) -> Result<Arc<Path>, PackageError> {
